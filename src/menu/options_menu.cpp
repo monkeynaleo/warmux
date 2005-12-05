b6c3907629820aeba84601424273e93a50e80930
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2005-12-05 23:47:10 +0000

    Make Options Menu compliant with the new GUI API. BUT the options menu is now broken because of misplaced widgets! TODO implement a vertical box to add widget in

--- src/menu/options_menu.cpp
+++ src/menu/options_menu.cpp
@@ -16,8 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  ******************************************************************************
- * Menu du jeu permettant de lancer une partie, modifier les options, d'obtenir
- * des informations, ou encore quitter le jeu.
+ * Options menu
  *****************************************************************************/
 
 #include "options_menu.h"
@@ -26,8 +25,9 @@
 #include "../gui/list_box.h"
 #include "../gui/check_box.h"
 #include "../gui/spin_button.h"
-#include "../tool/resource_manager.h"
+#include "../gui/question.h"
 
+#include "../tool/resource_manager.h"
 #include "../graphic/graphism.h"
 #include "../team/teams_list.h"
 #include "../game/game_mode.h"
@@ -38,14 +38,11 @@
 #include "../tool/i18n.h"
 #include "../sound/jukebox.h"
 #include "../tool/string_tools.h"
-#include "../gui/question.h"
+
 using namespace Wormux;
 using namespace std;
 
 //-----------------------------------------------------------------------------
-OptionMenu options_menu;
-//-----------------------------------------------------------------------------
-
 
 const uint PAUSE = 10;
 
@@ -90,113 +87,110 @@ OptionMenu::OptionMenu()
 {
   fin_boucle = false ;
   m_init = false;
-
-  valider = new Button(); 
-  enregistrer = new Button();
-  annuler = new Button();
    
-  lboxMaps = new ListBox(0, 0, MAPS_LARG, MAPS_HAUT);
-  lboxTeams = new ListBox(0, 0, TEAMS_LARG, TEAMS_HAUT);
-  lboxVideoMode = new ListBox(0, 0, VIDEO_MODE_LARG, VIDEO_MODE_HAUT);
-  lboxSoundFreq = new ListBox(0, 0, SOUND_FREQ_WIDTH, SOUND_FREQ_HEIGHT);
+  uint x = (video.GetWidth()/2);
+  uint y = video.GetHeight()-50;
 
-  option_display_wind_particles = new CheckBox();
-  option_affichage_energie = new CheckBox();
-  option_affichage_nom = new CheckBox();
-  full_screen = new CheckBox();
-  opt_music = new CheckBox();
-  opt_sound_effects = new CheckBox();
+  Profile *res = resource_manager.LoadXMLProfile( "graphism.xml");
 
-  option_temps_tour = new SpinButton();
-  option_temps_fin_tour = new SpinButton();
-  option_nb_ver = new SpinButton();
-  option_energie_ini = new SpinButton();
+  valider = new Button(x-50, y, res, "menu/valider"); 
+  enregistrer = new Button(x, y, res, "menu/enregistrer");
+  annuler = new Button(x+50, y, res, "menu/annuler");
+   
+  lboxMaps = new ListBox(x, MAPS_Y, MAPS_LARG, MAPS_HAUT);
+  lboxTeams = new ListBox(500, TEAMS_Y, TEAMS_LARG, TEAMS_HAUT);
+  lboxVideoMode = new ListBox(500, VIDEO_MODE_Y, VIDEO_MODE_LARG, VIDEO_MODE_HAUT);
+  lboxSoundFreq = new ListBox(500, SOUND_FREQ_Y, SOUND_FREQ_WIDTH, SOUND_FREQ_HEIGHT);
 
+
+  x = (video.GetWidth()-CARTE_LARG-MAPS_LARG-TEAMS_LARG)/4;
+
+  option_display_wind_particles = new CheckBox(_("Display wind particles?"), x, CBOX_ENERGIE_Y, MAPS_LARG);
+  option_affichage_energie = new CheckBox(_("Display player energy?"), x, CBOX_ENERGIE_Y+20, MAPS_LARG);
+  option_affichage_nom = new CheckBox(_("Display player's name?"), x, CBOX_ENERGIE_Y+2*20, MAPS_LARG);
+  full_screen = new CheckBox(_("Fullscreen?"), x, CBOX_ENERGIE_Y+7*20, MAPS_LARG);
+
+  opt_music = new CheckBox(_("Music?"), video.GetWidth()/2, 0, MAPS_LARG);
+  opt_sound_effects = new CheckBox(_("Sound effects?"), video.GetWidth()/2, 0, MAPS_LARG);
+
+  option_temps_tour = new SpinButton(_("Duration of a turn:"), x, CBOX_ENERGIE_Y+(3*20),
+				     60, 5, TPS_TOUR_MIN, TPS_TOUR_MAX);
+
+  option_temps_fin_tour = new SpinButton(_("Duration of the end of a turn:"), x, CBOX_ENERGIE_Y+(4*20),
+					 2, 1, TPS_FIN_TOUR_MIN, TPS_FIN_TOUR_MAX);
+
+  option_nb_ver = new SpinButton(_("Number of players per team:"), x, CBOX_ENERGIE_Y+(5*20),
+				 4, 1, NBR_VER_MIN, NBR_VER_MAX);
+
+  option_energie_ini = new SpinButton(_("Initial energy:"), x, CBOX_ENERGIE_Y+(6*20) ,
+				      100, 5, 50, 200);
+
+
+
+
+  uint centre_x = video.GetWidth()/2;
 }
 
 //-----------------------------------------------------------------------------
 
-#ifdef CL
-
-void OptionMenu::SignalWM_QUIT ()
-{ 
-   fin_boucle = true;
-}
-
-//-----------------------------------------------------------------------------
-
-//Traitement d'une touche clavier relachée
-
-void OptionMenu::TraiteTouche (const CL_InputEvent &touche)
+OptionMenu::~OptionMenu()
 {
-  if (touche.id == CL_KEY_ESCAPE)
-  {
-    fin_boucle = true;
-    return;
-  }
+  fin_boucle = false ;
+  m_init = false;
 
-  if (touche.id == CL_KEY_ENTER)
-  {
-    EnregistreOptions();
-    fin_boucle = true;
-    return;
-  }
+  delete valider ;
+  delete enregistrer;
+  delete annuler;
+   
+  delete lboxMaps;
+  delete lboxTeams;
+  delete lboxVideoMode;
+  delete lboxSoundFreq;
+
+  delete option_display_wind_particles;
+  delete option_affichage_energie;
+  delete option_affichage_nom;
+  delete full_screen;
+  delete opt_music;
+  delete opt_sound_effects;
+
+  delete option_temps_tour;
+  delete option_temps_fin_tour;
+  delete option_nb_ver;
+  delete option_energie_ini;
+
 }
-
-#else
-// NOTHING
-#endif
 
 //-----------------------------------------------------------------------------
 
-#ifdef CL
-void OptionMenu::TraiteClic (const CL_InputEvent &event)
-{
-  if (event.id == CL_MOUSE_LEFT)
-  {
-    int x = CL_Mouse::get_x();
-    int y = CL_Mouse::get_y();
-#else
 void OptionMenu::onClick ( int x, int y)
 {     
-#endif
-     
-    if (valider->Test (x, y)) {
-      jukebox.Play("share", "menu/ok");
-      EnregistreOptions();
-      fin_boucle = true;
-    } else if (annuler->Test (x, y)) {
-      jukebox.Play("share", "menu/cancel");
-      fin_boucle = true;
-    } else if (enregistrer->Test (x, y)) {
-      EnregistreOptions();
-    } else if (lboxMaps->Clic(x, y)) {
-       ChangeTerrain();
-    } else if (lboxVideoMode->Clic(x, y)) {
-    } else if (full_screen->Clic(x, y)) {
-    } else if (lboxTeams->Clic(x, y)) {
-    } else if (lboxSoundFreq->Clic(x,y)) {
-    } else if (option_display_wind_particles->Clic(x, y)) {
-    } else if (option_affichage_energie->Clic(x, y)) {
-    } else if (option_affichage_nom->Clic(x, y)) {
-    } else if (option_temps_tour->Clic(x, y)) {
-    } else if (option_temps_fin_tour->Clic(x, y)) {
-    } else if (option_nb_ver->Clic(x, y)) {
-    } else if (option_energie_ini->Clic(x, y)) {
-    } else if (opt_music->Clic (x,y)) {
-    } else if (opt_sound_effects->Clic (x,y)) {
-    }
-    return;
+  if (valider->MouseIsOver (x, y)) {
+    jukebox.Play("share", "menu/ok");
+    EnregistreOptions();
+    fin_boucle = true;
+  } else if (annuler->MouseIsOver (x, y)) {
+    jukebox.Play("share", "menu/cancel");
+    fin_boucle = true;
+  } else if (enregistrer->MouseIsOver (x, y)) {
+    EnregistreOptions();
+  } else if (lboxMaps->Clic(x, y)) {
+    ChangeTerrain();
+  } else if (lboxVideoMode->Clic(x, y)) {
+  } else if (full_screen->Clic(x, y)) {
+  } else if (lboxTeams->Clic(x, y)) {
+  } else if (lboxSoundFreq->Clic(x,y)) {
+  } else if (option_display_wind_particles->Clic(x, y)) {
+  } else if (option_affichage_energie->Clic(x, y)) {
+  } else if (option_affichage_nom->Clic(x, y)) {
+  } else if (option_temps_tour->Clic(x, y)) {
+  } else if (option_temps_fin_tour->Clic(x, y)) {
+  } else if (option_nb_ver->Clic(x, y)) {
+  } else if (option_energie_ini->Clic(x, y)) {
+  } else if (opt_music->Clic (x,y)) {
+  } else if (opt_sound_effects->Clic (x,y)) {
+  }
 }
-
-//-----------------------------------------------------------------------------
-
-#ifdef CL
-void OptionMenu::RetourMenuPrincipal ()
-{
-  fin_boucle = true;
-}
-#endif
 
 //-----------------------------------------------------------------------------
 
@@ -211,20 +205,9 @@ void OptionMenu::Init ()
 #else
    // WHY app.run() ????
 #endif
+
   espace = (video.GetWidth()-CARTE_LARG-MAPS_LARG-TEAMS_LARG)/4;
 
-#ifdef CL
-  fond_option = new CL_Surface("menu/fond_option", graphisme.LitRes());
-  fond_maps = new CL_Surface("menu/fond_maps", graphisme.LitRes());
-  fond_box = new CL_Surface("menu/fond_box", graphisme.LitRes());
-  fond_box2 = new CL_Surface("menu/fond_box2", graphisme.LitRes());
-
-  // Chargement des images des boutons
-  valider->SetImage ("menu/valider", graphisme.LitRes());
-  enregistrer->SetImage ("menu/enregistrer", graphisme.LitRes());
-  annuler->SetImage("menu/annuler", graphisme.LitRes());
-
-#else
   Profile *res = resource_manager.LoadXMLProfile( "graphism.xml");
 
   fond_option = new Sprite( resource_manager.LoadImage( res, "menu/fond_option"));
@@ -232,37 +215,6 @@ void OptionMenu::Init ()
   fond_box = resource_manager.LoadImage( res, "menu/fond_box");
   fond_box2 = resource_manager.LoadImage( res, "menu/fond_box2");
 
-  // Chargement des images des boutons
-  valider->SetImage (res, "menu/valider");
-  enregistrer->SetImage ( res, "menu/enregistrer");
-  annuler->SetImage( res, "menu/annuler");
-
-#endif
-   
-  // CheckBox
-  option_display_wind_particles->Init (_("Display wind particles?"), espace, 0);
-  option_affichage_energie->Init (_("Display player energy?"), espace, 0);
-  option_affichage_nom->Init (_("Display player's name?"), espace, 0);
-  full_screen->Init (_("Fullscreen?"), 0, 0);
-
-  // SpinButton
-  option_temps_tour->Init (_("Duration of a turn:"), 0, 0,
-			  60, 5, TPS_TOUR_MIN, TPS_TOUR_MAX);
-  option_temps_fin_tour->Init (_("Duration of the end of a turn:"), 0, 0,
-			   2, 1, TPS_FIN_TOUR_MIN, TPS_FIN_TOUR_MAX);
-  option_nb_ver->Init(_("Number of players per team:"), espace, 0,
-		     4, 1, NBR_VER_MIN, NBR_VER_MAX);
-  option_energie_ini->Init(_("Initial energy:"), espace, 0, 100, 5, 50, 200);
-
-  opt_music->Init (_("Music?"), video.GetWidth()/2, 0);
-  opt_sound_effects->Init (_("Sound effects?"), video.GetWidth()/2, 0);
-
-  // ListBox
-  lboxMaps->Init ();
-  lboxTeams->Init ();
-  lboxVideoMode->Init ();
-  lboxSoundFreq->Init ();
-  
   // Load Maps' list
   std::sort(lst_terrain.liste.begin(), lst_terrain.liste.end(), compareMaps);
 
@@ -292,18 +244,6 @@ void OptionMenu::Init ()
   }
 
   //Generate video mode list
-#ifdef BUGGY_CODE
-  Video::ConstIteratorMode
-    mode=video.GetModes().begin(),
-    mode_end=video.GetModes().end();
-  for (int index=0; mode != mode_end; ++mode, ++index)
-  {
-    lboxVideoMode->AddItem (index==video.GetCurrentMode(),
-			   mode->get_string(),
-			   mode->get_string());
-  }
-#endif
-#ifndef CL
   SDL_Rect **modes;
 
   /* Get available fullscreen/hardware modes */
@@ -325,7 +265,6 @@ void OptionMenu::Init ()
 	lboxVideoMode->AddItem(false, ss.str(), ss.str());
     }
   }
-#endif
 
   // Generate sound mode list
   uint current_freq = jukebox.GetFrequency();
@@ -349,13 +288,9 @@ void OptionMenu::Reset()
   option_nb_ver->SetValue(game_mode.max_characters);
   option_energie_ini->SetValue(game_mode.character.init_energy);
 
-#ifdef CL
-  opt_music->SetValue( jukebox.GetMusicConfig() );
-  opt_sound_effects->SetValue( jukebox.GetEffectsConfig() );
-#else
+
   opt_music->SetValue( jukebox.UseMusic() );
   opt_sound_effects->SetValue( jukebox.UseEffects() );
-#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -375,26 +310,16 @@ void OptionMenu::EnregistreOptions()
   game_mode.max_characters = option_nb_ver->GetValue() ;
 
   game_mode.character.init_energy = option_energie_ini->GetValue() ;
-#ifdef CL
-  video.ChangeMode(lboxVideoMode->GetSelectedItem(),full_screen->GetValue());
-#else
+
+  // Video mode
   uint mode = lboxVideoMode->GetSelectedItem();
   std::string s_mode = lboxVideoMode->ReadValue(mode);
   int w, h;
   sscanf(s_mode.c_str(),"%dx%d", &w, &h);
   video.SetConfig(w, h, full_screen->GetValue());
-   // TODO
-#endif
+
    
   // Sound
-#ifdef CL
-  jukebox.ActiveSound( opt_sound->GetValue() );
-  jukebox.ActiveMusic( opt_music->GetValue() );
-  jukebox.ActiveEffects( opt_sound_effects->GetValue() );
-  std::string sfreq = lboxSoundFreq.ReadValue(lboxSoundFreq.GetSelectedItem());
-  long freq;
-  if (str2long(sfreq,freq)) jukebox.SetFrequency (freq);
-#else
   jukebox.ActiveMusic( opt_music->GetValue() );
   jukebox.ActiveEffects( opt_sound_effects->GetValue() );
   std::string sfreq = lboxSoundFreq->ReadValue(lboxSoundFreq->GetSelectedItem());
@@ -402,7 +327,6 @@ void OptionMenu::EnregistreOptions()
   if (str2long(sfreq,freq)) jukebox.SetFrequency (freq);
   
   jukebox.Init(); // commit modification on sound options
-#endif
    
   //Enregistre le tout dans le XML
   config.Sauve();
@@ -410,23 +334,6 @@ void OptionMenu::EnregistreOptions()
 
 //-----------------------------------------------------------------------------
 
-#ifdef CL
-void OptionMenu::ChangeTerrain()
-{
-  std::string map_id = lboxMaps->ReadLabel(lboxMaps->GetSelectedItem());
-  uint map = lst_terrain.FindMapById(map_id);
-  map_preview = lst_terrain.liste[map].preview;
-  float scale = std::min( float(CARTE_HAUT)/map_preview.get_height(), 
-                          float(CARTE_LARG)/map_preview.get_width() ) ;
-  carte_haut = (uint)(map_preview.get_height() * scale);
-  carte_larg = (uint)(map_preview.get_width() * scale);
-
-  map_preview.set_scale (scale, scale);
-  
-  carte_x = (espace*2+MAPS_LARG)+(CARTE_LARG-carte_larg)/2;
-  carte_y = CARTE_Y+(CARTE_HAUT-carte_haut)/2;
-}
-#else
 void OptionMenu::ChangeTerrain()
 {
   std::string map_id = lboxMaps->ReadLabel(lboxMaps->GetSelectedItem());
@@ -442,7 +349,6 @@ void OptionMenu::ChangeTerrain()
   carte_x = (espace*2+MAPS_LARG)+(CARTE_LARG-carte_larg)/2;
   carte_y = CARTE_Y+(CARTE_HAUT-carte_haut)/2;
 }
-#endif
 
 //-----------------------------------------------------------------------------
 
@@ -456,54 +362,13 @@ void OptionMenu::Lance ()
   Init();
   Reset();
 
-#ifdef CL
-  //Installe le pilote clavier
-  keyboard_slot = CL_Keyboard::sig_key_up().connect(this, &OptionMenu::TraiteTouche);
-  mouse_slot = CL_Mouse::sig_key_up().connect(this, &OptionMenu::TraiteClic);
-  slot_quit = app.clwindow -> sig_window_close().connect(this, &OptionMenu::SignalWM_QUIT);
-#endif
-   
-  x = (video.GetWidth()/2);
-  y = video.GetHeight()-50;
-  valider->SetPos (x-50, y);
-  enregistrer->SetPos (x, y);
-  annuler->SetPos (x+50, y);
-
   espace = (video.GetWidth()-CARTE_LARG-MAPS_LARG-TEAMS_LARG)/4;
   //espace_h = (video.GetHeight()-CARTE_HAUT-30-100)/4;
   uint centre_x = video.GetWidth()/2;
 
-  lboxMaps->SetXY (espace, 30);
-  lboxTeams->SetXY ((espace*3)+CARTE_LARG+MAPS_LARG, TEAMS_Y);
-  lboxVideoMode->SetXY ((espace*3)+CARTE_LARG+MAPS_LARG-30, VIDEO_MODE_Y);
-
-  x = espace;
-  option_affichage_energie->SetXY (x, CBOX_ENERGIE_Y);
-  option_affichage_nom->SetXY (x, CBOX_ENERGIE_Y+20);
-  option_display_wind_particles->SetXY (x, CBOX_ENERGIE_Y+(2*20));
-  option_temps_tour->SetXY (x, CBOX_ENERGIE_Y+(3*20));
-  option_temps_fin_tour->SetXY (x, CBOX_ENERGIE_Y+(4*20));
-  option_nb_ver->SetXY (x, CBOX_ENERGIE_Y+(5*20));
-  option_energie_ini->SetXY (x, CBOX_ENERGIE_Y+(6*20));
-
-  x = centre_x-50;
-  y = FULL_SCREEN_Y;
-  full_screen->SetXY (x, y);
-  y += 20; opt_music->SetXY (x, y);
-  y += 20; opt_sound_effects->SetXY (x, y);
-
-  lboxSoundFreq->SetXY (x, SOUND_FREQ_Y);
-
   fin_boucle = false;
   do
   {
-#ifdef CL
-    //Lit la position de la souris
-    x = CL_Mouse::get_x();
-    y = CL_Mouse::get_y();
-
-    CL_Display::clear();
-#else
    // Poll and treat events
 	
    SDL_Event event;
@@ -537,9 +402,7 @@ void OptionMenu::Lance ()
 	  }
      }
 
-     SDL_GetMouseState( &x, &y);
-     
-#endif
+   SDL_GetMouseState( &x, &y);
      
     int nv_equipe = lboxTeams->MouseIsOnWitchItem (x,y);
     if (nv_equipe != -1)
@@ -548,13 +411,6 @@ void OptionMenu::Lance ()
     }
       
     // affichage des boutons et de la carte
-#ifdef CL
-    fond_option->draw(CL_Rect(0, 0, video.GetWidth(), video.GetHeight()));
-    fond_maps->draw(espace-30, 5);
-    fond_maps->draw((espace*3)+CARTE_LARG+MAPS_LARG-30, 5);
-    fond_box->draw(espace-30, CBOX_ENERGIE_Y-20);
-    fond_box2->draw((espace*3)+CARTE_LARG+MAPS_LARG-100, VIDEO_MODE_Y-25);
-#else
     fond_option->ScaleSize(app.sdlwindow->w, app.sdlwindow->h);
     fond_option->Blit( app.sdlwindow, 0, 0);
     SDL_Rect r1 = {espace-30,5,fond_maps->w,fond_maps->h};	  
@@ -565,38 +421,31 @@ void OptionMenu::Lance ()
     SDL_BlitSurface( fond_box, NULL, app.sdlwindow, &r3);
     SDL_Rect r4 = {(espace*3)+CARTE_LARG+MAPS_LARG-100, VIDEO_MODE_Y-25,fond_box->w,fond_box->h};
     SDL_BlitSurface( fond_box2, NULL, app.sdlwindow, &r4);
-#endif
      
     valider->Draw(x,y) ;
     annuler->Draw(x,y) ;
     enregistrer->Draw(x,y) ;
-    lboxMaps->Display(x,y);
-    lboxTeams->Display(x,y);
-    lboxVideoMode->Display(x,y);
-    full_screen->Display(x,y);
-    lboxSoundFreq->Display(x,y);
-    option_affichage_energie->Display(x,y);
-    option_affichage_nom->Display(x,y);
-    option_display_wind_particles->Display(x,y);
-    option_temps_tour->Display(x,y);
-    option_temps_fin_tour->Display(x,y);
-    option_nb_ver->Display(x,y);
-    option_energie_ini->Display(x,y);
-    opt_music->Display(x,y);
-    opt_sound_effects->Display(x,y);
+    lboxMaps->Draw(x,y);
+    lboxTeams->Draw(x,y);
+    lboxVideoMode->Draw(x,y);
+    full_screen->Draw(x,y);
+    lboxSoundFreq->Draw(x,y);
+    option_affichage_energie->Draw(x,y);
+    option_affichage_nom->Draw(x,y);
+    option_display_wind_particles->Draw(x,y);
+    option_temps_tour->Draw(x,y);
+    option_temps_fin_tour->Draw(x,y);
+    option_nb_ver->Draw(x,y);
+    option_energie_ini->Draw(x,y);
+    opt_music->Draw(x,y);
+    opt_sound_effects->Draw(x,y);
 
-#ifdef CL
-    derniere_equipe -> ecusson.draw ((espace*3)+CARTE_LARG+MAPS_LARG+
-				     (TEAMS_LARG/2)-ECUSSON_LARG/2 ,
-				     ECUSSON_Y);
-#else
+
    SDL_Rect team_icon_rect = { (espace*3)+CARTE_LARG+MAPS_LARG+(TEAMS_LARG/2)-ECUSSON_LARG/2,
 	                       ECUSSON_Y,
                                ECUSSON_LARG,
                                ECUSSON_HAUT};
    SDL_BlitSurface (derniere_equipe->ecusson, NULL, app.sdlwindow, &team_icon_rect); 
-     
-#endif
      
     if (!terrain_init)
     {
@@ -604,28 +453,12 @@ void OptionMenu::Lance ()
       ChangeTerrain();
     }
 
-#ifdef CL
-    map_preview.draw (carte_x, carte_y);
-      
-    CL_Display::flip();
-
-    CL_System::keep_alive (PAUSE) ;
-#else
     map_preview->Blit ( app.sdlwindow, carte_x, carte_y);     
      
     SDL_Flip( app.sdlwindow);
-#endif
      
   } while (!fin_boucle);
 
-#ifdef CL
-//  Désinstalle le pilote
-  CL_Keyboard::sig_key_up().disconnect(keyboard_slot);
-  CL_Mouse::sig_key_up().disconnect(mouse_slot);
-#ifdef PORT_CL07
-  CL_DisplayWindow::sig_window_close().disconnect(slot_quit);
-#endif
-#endif
 }
 
 //-----------------------------------------------------------------------------
