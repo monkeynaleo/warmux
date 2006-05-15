6ad658b44ee132564fee95ba5277a2bddefa0393
oznog <oznog@30ef787d-52ff-0310-b286-e08351bb7647>
2006-05-15 21:13:48 +0000

    A lot of trailing spaces removed but the purpose was in fact   GameMenu::~GameMenu()   {  +  delete map_preview;     delete map_box;     delete team_box;   }   (Yet another memleak removed)

--- src/menu/game_menu.cpp
+++ src/menu/game_menu.cpp
@@ -40,15 +40,15 @@ const uint TEAM_LOGO_H = 48;
 
 const uint MAPS_X = 20;
 const uint MAPS_W = 160;
- 
+
 const uint MAP_PREVIEW_W = 300;
 
 GameMenu::GameMenu() :
   Menu("menu/bg_play")
-{  
+{
   Profile *res = resource_manager.LoadXMLProfile( "graphism.xml",false);
   Rectanglei rectZero(0, 0, 0, 0);
-  
+
   normal_font = Font::GetInstance(Font::FONT_NORMAL);
 
   // Center the boxes!
@@ -70,7 +70,7 @@ GameMenu::GameMenu() :
 
   bt_add_team = new Button( Rectanglei(0, 0, 48, 48) ,res,"menu/arrow-right");
   buttons_tmp_box->AddWidget(bt_add_team);
-  
+
   bt_remove_team = new Button( Rectanglei( 0, 0, 48, 48 ),res,"menu/arrow-left");
   buttons_tmp_box->AddWidget(bt_remove_team);
 
@@ -78,13 +78,13 @@ GameMenu::GameMenu() :
   buttons_tmp_box->AddWidget(space_for_logo);
 
   tmp_box->AddWidget(buttons_tmp_box);
-  lbox_selected_teams = new ListBox( Rectanglei(0, 0, TEAMS_W, TEAMS_H - TEAM_LOGO_H - 5 )); 
+  lbox_selected_teams = new ListBox( Rectanglei(0, 0, TEAMS_W, TEAMS_H - TEAM_LOGO_H - 5 ));
   lbox_selected_teams->always_one_selected = false;
   tmp_box->AddWidget(lbox_selected_teams);
 
   team_box->AddWidget(tmp_box);
 
-  
+
 
   /* Choose the map !! */
   tmp_box = new HBox( Rectanglei(0, 0, 1, MAP_PREVIEW_W - 25 ), false);
@@ -94,7 +94,7 @@ GameMenu::GameMenu() :
   lbox_maps = new ListBox( Rectanglei(0, 0, MAPS_W, MAP_PREVIEW_W-25 ));
   tmp_box->AddWidget(lbox_maps);
   tmp_box->AddWidget(new NullWidget( Rectanglei(0, 0, MAP_PREVIEW_W+5, MAP_PREVIEW_W)));
-  
+
   map_box = new VBox( Rectanglei(x, team_box->GetPositionY()+team_box->GetSizeY()+20, 475, 1) );
   map_box->AddWidget(new Label(_("Select the world:"), rectZero, *normal_font));
   map_box->AddWidget(tmp_box);
@@ -116,7 +116,7 @@ GameMenu::GameMenu() :
   teams_list.full_list.sort(compareTeams);
 
   TeamsList::full_iterator
-    it=teams_list.full_list.begin(), 
+    it=teams_list.full_list.begin(),
     end=teams_list.full_list.end();
 
   uint i=0;
@@ -135,12 +135,13 @@ GameMenu::GameMenu() :
 
 GameMenu::~GameMenu()
 {
+  delete map_preview;
   delete map_box;
   delete team_box;
 }
 
 void GameMenu::OnClic(const Point2i &mousePosition, int button)
-{     
+{
   if (lbox_maps->Clic(mousePosition, button)) {
     ChangeMap();
   } else if (lbox_all_teams->Clic(mousePosition, button)) {
@@ -149,7 +150,7 @@ void GameMenu::OnClic(const Point2i &mousePosition, int button)
 
   } else if ( bt_add_team->Contains(mousePosition)) {
     if (lbox_selected_teams->GetItemsList()->size() < GameMode::GetInstance()->max_teams)
-      MoveTeams(lbox_all_teams, lbox_selected_teams, false); 
+      MoveTeams(lbox_all_teams, lbox_selected_teams, false);
   } else if ( bt_remove_team->Contains(mousePosition)) {
     MoveTeams(lbox_selected_teams, lbox_all_teams, true);
   }
@@ -162,14 +163,14 @@ void GameMenu::SaveOptions()
   lst_terrain.ChangeTerrainNom (map_id);
 
   // teams
-  std::vector<list_box_item_t> * 
+  std::vector<list_box_item_t> *
     selected_teams = lbox_selected_teams->GetItemsList();
 
   if (selected_teams->size() > 1) {
     std::list<uint> selection;
 
-    std::vector<list_box_item_t>::iterator 
-      it = selected_teams->begin(), 
+    std::vector<list_box_item_t>::iterator
+      it = selected_teams->begin(),
       end = selected_teams->end();
 
     int index = -1;
@@ -181,7 +182,7 @@ void GameMenu::SaveOptions()
     teams_list.ChangeSelection (selection);
 
   }
-   
+
   //Save options in XML
   Config::GetInstance()->Save();
 }
@@ -204,7 +205,7 @@ void GameMenu::ChangeMap()
   if(terrain_init)
     delete map_preview;
   map_preview = new Sprite(lst_terrain.liste[map].preview);
-  float scale = std::min( float(MAP_PREVIEW_W)/map_preview->GetHeight(), 
+  float scale = std::min( float(MAP_PREVIEW_W)/map_preview->GetHeight(),
                           float(MAP_PREVIEW_W)/map_preview->GetWidth() ) ;
 
   map_preview->Scale (scale, scale);
@@ -213,23 +214,23 @@ void GameMenu::ChangeMap()
 void GameMenu::MoveTeams(ListBox * from, ListBox * to, bool sort)
 {
   if (from->GetSelectedItem() != -1) {
-    to->AddItem (false, 
+    to->AddItem (false,
 		 from->ReadLabel(),
 		 from->ReadValue());
     to->Deselect();
     if (sort) to->Sort();
-    
+
     from->RemoveSelected();
   }
 }
 
 void GameMenu::Draw(const Point2i &mousePosition)
-{   
+{
   Team* last_team = teams_list.FindByIndex(0);
-   
+
   map_box->Draw(mousePosition);
   team_box->Draw(mousePosition);
-     
+
   int t = lbox_all_teams->MouseIsOnWhichItem(mousePosition);
   if (t != -1) {
     int index = -1;
@@ -243,17 +244,17 @@ void GameMenu::Draw(const Point2i &mousePosition)
       if (new_team!=NULL) last_team = new_team;
     }
   }
-   
+
   AppWormux * app = AppWormux::GetInstance();
   app->video.window.Blit( last_team->ecusson, space_for_logo->GetPosition() );
-  
+
   if (!terrain_init){
       ChangeMap();
       terrain_init = true;
   }
-  
-  map_preview->Blit ( app->video.window, 
-		      map_box->GetPositionX()+MAPS_W+10, 
+
+  map_preview->Blit ( app->video.window,
+		      map_box->GetPositionX()+MAPS_W+10,
 		      map_box->GetPositionY()+map_box->GetSizeY()/2-map_preview->GetHeight()/2);
 }
 
