7141125d71548126ba3dbbe05bce1064257a851d
lodesi <lodesi@30ef787d-52ff-0310-b286-e08351bb7647>
2006-06-03 14:20:56 +0000

    remove unneeded files

--- data/map/leafs/install.sh
+++
@@ -1,5 +0,0 @@
-mkdir /usr/share/games/wormux/map/leafs/
-cp birds.png front.png back.png preview.jpg config.xml /usr/share/games/wormux/map/leafs/
-chown -R root:games /usr/share/games/wormux/map/leafs
-chmod -R o-rwx /usr/share/games/wormux/map/leafs
-chmod -x /usr/share/games/wormux/map/leafs/*
