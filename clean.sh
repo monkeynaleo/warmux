393490e6beeb1ddcaf9a9df859587880eadef9c2
haypo <haypo@30ef787d-52ff-0310-b286-e08351bb7647>
2006-08-15 22:04:14 +0000

    clean.sh: Don't remove map-generator Makefile (not generated)

--- clean.sh
+++ clean.sh
@@ -4,5 +4,5 @@ echo "Clean autogen things"
 rm -f config.log config.status mkinstalldirs configure aclocal.m4
 rm -rf autom4te.cache
 rm -rf src/.deps
-find -name "Makefile" -exec rm {} \;
+find . -path "*/map-generator*" -o -name "Makefile" -exec rm {} \;
 for i in $(find -name "Makefile.in" | grep -v '^./intl/'); do rm "$i"; done
