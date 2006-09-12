7cef2d97949cb69c5e136f2c4ad6298e00c1257e
drayan <drayan@30ef787d-52ff-0310-b286-e08351bb7647>
2006-09-12 17:43:34 +0000

    And the anvil.h anvil.cpp.

---
+++ src/weapon/anvil.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ *  Wormux, a free clone of the game Worms from Team17.
+ *  Copyright (C) 2001-2004 Lawrence Azzoug.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ ******************************************************************************
+ * Anvil : appear in top of an enemy and crush down his head
+ *****************************************************************************/
+
+#ifndef ANVIL_H
+#define ANVIL_H
+
+#include <SDL.h>
+#include "../include/base.h"
+#include "../graphic/surface.h"
+#include "../object/physical_obj.h"
+#include "launcher.h"
+
+class Anvil : public WeaponProjectile
+{
+  public:
+    Anvil(ExplosiveWeaponConfig& cfg,
+          WeaponLauncher * p_launcher);
+  protected:
+    void SignalObjectCollision(PhysicalObj * obj);
+};
+
+class AnvilLauncher : public WeaponLauncher
+{
+  public:
+    AnvilLauncher();
+  protected:
+    WeaponProjectile * GetProjectileInstance();
+};
+
+#endif /* ANVIL_H */
