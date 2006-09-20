bd4e94c59b405db19c577c0831ce3f193b21fe3a
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2006-09-20 22:54:58 +0000

    Continue to work on Options menu!
    -New interior color for boxes
    -New ListBoxWithLabel widget
    -Bigger font size for text display in options menu

--- src/graphic/colors.h
+++ src/graphic/colors.h
@@ -47,8 +47,11 @@ const Color primary_red_color(255, 0, 0, SDL_ALPHA_OPAQUE);
 const Color defaultColorBox(80, 80, 159, 206);
 const Color defaultColorRect(49, 32, 122, 255);
 
-const Color defaultOptionColorBox(34, 133, 175, 206);
+const Color defaultOptionColorBox(255,255,255,160);
+//const Color defaultOptionColorBox(34, 133, 175, 206);
 const Color highlightOptionColorBox(200, 100, 100, 206);
+//const Color defaultOptionColorBox(34, 133, 175, 206);
+
 const Color defaultDisabledColorBox(0,0,0,127);
 const Color defaultOptionColorRect(59,88,129,206);
 
