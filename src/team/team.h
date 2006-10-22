b4cc42bbdd45aaf3d94eba3cff2b0e90b37d4bc4
yekcim <yekcim@30ef787d-52ff-0310-b286-e08351bb7647>
2006-10-22 18:01:34 +0000

    No Worms (again)

--- src/team/team.h
+++ src/team/team.h
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  ******************************************************************************
- * A worms team.
+ * A team.
  *****************************************************************************/
 
 #ifndef TEAM_H
@@ -90,7 +90,7 @@ public:
   // Prepare turn.
   void PrepareTurn();
 
-  // Access to the worms.
+  // Access to the character.
   Character& ActiveCharacter();
 
   void DrawEnergy();
