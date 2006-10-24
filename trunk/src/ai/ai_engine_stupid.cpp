c714c9edc7c734b3cd7c2ec98d0dce9c55263068
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2006-10-24 22:24:43 +0000

    Fix some walking problems in the Artificial stupid engine

--- trunk/src/ai/ai_engine_stupid.cpp
+++ trunk/src/ai/ai_engine_stupid.cpp
@@ -20,7 +20,9 @@
  *****************************************************************************/
 
 #include "ai_engine_stupid.h"
+#include "../include/action_handler.h"
 #include "../character/body.h"
+#include "../character/move.h"
 #include "../network/randomsync.h"
 #include "../team/teams_list.h"
 
@@ -47,48 +49,106 @@ void AIStupidEngine::BeginTurn()
 
   m_begin_turn_time = Time::GetInstance()->Read();
   m_step = 0;
+  m_is_walking = false;
+
+  ChooseDirection();
 }
+
+void AIStupidEngine::ChooseDirection()
+{
+  m_goes_right = randomSync.GetBool();
+
+  ActiveCharacter().SetDirection(m_goes_right);
+}
+
+void AIStupidEngine::Walk()
+{
+  if (!m_is_walking) { 
+    ActiveCharacter().InitMouvementDG(100);
+    ActiveCharacter().body->StartWalk();
+  }
+
+  m_is_walking = true;
+
+  if(ActiveCharacter().IsImmobile()) {
+    if (m_goes_right)
+      MoveCharacterRight(ActiveCharacter());
+    else
+      MoveCharacterLeft(ActiveCharacter());
+  }
+}
+
+void AIStupidEngine::StopWalk()
+{
+  m_is_walking = false;
+  ActiveCharacter().body->StopWalk();
+}
+
 
 void AIStupidEngine::Refresh()
 {
   // new character to control
-  if (&ActiveCharacter() != m_last_char)
+  if (&ActiveCharacter() != m_last_char) 
     BeginTurn();
 
-  /* thinks about movement */
-  if ( m_step == 0 &&
-       Time::GetInstance()->Read() > m_begin_turn_time + 200) {
-    ActiveCharacter().SetDirection(randomSync.GetBool());
-    m_step++;
-  }
-    
-  if ( m_step == 1 &&
-       Time::GetInstance()->Read() > m_begin_turn_time + 400) {
-    ActiveCharacter().Jump();
-    m_step++;
-  }
-    
-  if ( m_step == 2 &&
-       Time::GetInstance()->Read() < m_begin_turn_time + 600) {
-    ActiveCharacter().InitMouvementDG(100);
-    ActiveCharacter().body->StartWalk();
-    m_step++;
-  }
+  // wait some seconds as if we are thinking...
+  if (Time::GetInstance()->Read() < m_begin_turn_time + 300)
+    return;
 
-  /* thinks about shooting */
-  if ( m_step == 3 &&
-       Time::GetInstance()->Read() > m_begin_turn_time + 1000) {
-    ActiveTeam().GetWeapon().NewActionShoot();
-    m_step++;
+  // walk !
+  if ( m_step == 0 ) {
+    Walk();
+
+    if (Time::GetInstance()->Read() > m_begin_turn_time + 600)
+      m_step++;
+
+    return;
   }
   
-  if ( m_step == 4 ) {
-    ActiveCharacter().SetDirection(randomSync.GetBool());
+  // jump !!
+  if ( m_step == 1 ) {
+    StopWalk();
+    ActionHandler::GetInstance()->NewAction (new Action(ACTION_JUMP));
     m_step++;
+
+    return;
+  }
+    
+  // walk
+  if ( m_step == 2 ) {
+    Walk();
+
+    if ( Time::GetInstance()->Read() > m_begin_turn_time + 1000 )
+      m_step++;
+
+    return;
   }
 
-  if ( m_step == 5 ) {
-    ActiveCharacter().InitMouvementDG(100);
-    ActiveCharacter().body->StartWalk();
+  // Shoot !!
+  if ( m_step == 3 ) {
+    StopWalk();
+    ActiveTeam().GetWeapon().NewActionShoot();
+    ActionHandler::GetInstance()->NewAction (new Action(ACTION_HIGH_JUMP));
+    m_step++;
+
+    return;
   }
+
+  // go go go !!
+  if ( m_step == 4 ) {
+    ChooseDirection();
+    m_step++;
+    return;
+  } 
+ 
+  if ( m_step == 5 ) {
+    ActionHandler::GetInstance()->NewAction (new Action(ACTION_HIGH_JUMP));
+    m_step++;
+    return;
+  }
+
+  if ( m_step == 6 ) {
+    Walk();
+  }
+
 }
