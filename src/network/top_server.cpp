b083f2f543e0f46db86198a24f66dff78c16ecfc
lodesi <lodesi@30ef787d-52ff-0310-b286-e08351bb7647>
2006-11-08 23:59:04 +0000

    Perform a handshake when connecting to a top server

--- src/network/top_server.cpp
+++ src/network/top_server.cpp
@@ -22,8 +22,11 @@
  
 #include <SDL_net.h>
 #include "top_server.h"
+#include "top_server_msg.h"
 #include "network.h"
 #include "gui/question.h"
+#include "include/app.h"
+#include "include/constant.h"
 #include "tool/debug.h"
 #include "tool/i18n.h"
 
@@ -43,6 +46,7 @@ TopServer::~TopServer()
     Disconnect();
 }
 
+/*************  Connection  /  Disconnection  ******************/
 bool TopServer::Connect()
 {
   assert(!connected);
@@ -50,13 +54,17 @@ bool TopServer::Connect()
   if( hidden_server )
     return true;
 
+  Question question;
+  question.Set(_("Contacting main server..."),1,0);
+  question.Draw();
+  AppWormux::GetInstance()->video.Flip();
+
   network.Init();
 
   MSG_DEBUG("top_server", "Opening connection");
 
-  if(SDLNet_ResolveHost(&ip, "localhost" , 1234)==-1)
+  if( SDLNet_ResolveHost(&ip, "localhost" , 1234) == -1 )
   {
-    Question question;
     question.Set(_("Invalid top server adress!"),1,0);
     question.Ask();
     printf("SDLNet_ResolveHost: %s\n", SDLNet_GetError());
@@ -67,7 +75,6 @@ bool TopServer::Connect()
 
   if(!socket)
   {
-    Question question;
     question.Set(_("Unable to contact top server!"),1,0);
     question.Ask();
     printf("SDLNet_ResolveHost: %s\n", SDLNet_GetError());
@@ -75,8 +82,10 @@ bool TopServer::Connect()
   }
 
   connected = true;
-  return true;
+
+  return HandShake();
 }
+
 
 void TopServer::Disconnect()
 {
@@ -89,6 +98,80 @@ void TopServer::Disconnect()
   if( !connected )
     return;
 
+  MSG_DEBUG("top_server", "Closing connection");
+
   SDLNet_TCP_Close(socket);
   connected = false;
 }
+
+/*************  Basic transmissions  ******************/
+void TopServer::Send(const int& nbr)
+{
+  char packet[4];
+  // this is not cute, but we don't want an int -> uint conversion here
+  Uint32 u_nbr = *((Uint32*)&nbr);
+
+  SDLNet_Write32(u_nbr, packet);
+  SDLNet_TCP_Send(socket, packet, sizeof(packet));
+}
+
+void TopServer::Send(const std::string &str)
+{
+  Send(str.size());
+  SDLNet_TCP_Send(socket, str.c_str(), str.size());  
+}
+
+int TopServer::ReceiveInt()
+{
+  char packet[4];
+  if( SDLNet_TCP_Recv(socket, packet, sizeof(packet)) < 1 )
+  {
+    Disconnect();
+    return 0;
+  }
+
+  Uint32 u_nbr = SDLNet_Read32(packet);
+  int nbr = *((int*)&u_nbr);
+  return nbr;
+}
+
+std::string TopServer::ReceiveStr()
+{
+  int size = ReceiveInt();
+
+  if(!connected)
+    return "";
+
+  assert(size > 0);
+  char* str = new char[size+1];
+
+  if( SDLNet_TCP_Recv(socket, str, size) < 1 )
+  {
+    Disconnect();
+    return "";
+  }
+
+  str[size] = '\0';
+
+  std::string st(str);
+  delete []str;
+  return st;
+}
+
+bool TopServer::HandShake()
+{
+  Send(TS_MSG_VERSION);
+  Send(Constants::VERSION);
+
+  std::string sign = ReceiveStr();
+  if(sign != "MassMurder!")
+  {
+    Question question;
+    question.Set(_("It doesn't seem to be a valid Wormux server..."),1,0);
+    question.Ask();
+    Disconnect();
+    return false;
+  }
+  return true;
+}
+
--- tools/top_serveur/client.h
+++ tools/top_serveur/client.h
@@ -1,11 +1,28 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 #include <netinet/in.h>
+#include <string>
+#include "../../src/network/top_server_msg.h"
 
 class Client
 {
 	int fd;
+	int received;
 	struct sockaddr_in address;
+	bool handshake_done;
+
+	char* str;
+	unsigned int str_size;
+
+	enum TopServerMsg msg_id;
+
+	// Return false if the client closed the connection
+	bool ReceiveStr(std::string & full_str);
+	bool ReceiveInt(int & nbr);
+
+	void SendInt(const int & nbr);
+	void SendStr(const std::string & full_str);
+	void SendSignature();
 public:
 	Client(int client_fd,struct sockaddr_in client_address);
 	~Client();
