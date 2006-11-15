30da91d7dda8a14e3e8f9f1358d76c14a0a0144d
lodesi <lodesi@30ef787d-52ff-0310-b286-e08351bb7647>
2006-11-15 15:10:41 +0000

    Reject connections instead of exiting when the max have been reached

--- trunk/tools/top_serveur/main.cpp
+++ trunk/tools/top_serveur/main.cpp
@@ -71,8 +71,7 @@ int main(int argc, void** argv)
 		if ( select(FD_SETSIZE, &acting_sock_set, NULL, NULL, NULL) < 1 )
 			TELL_ERROR;
 
-		// Find the socket where activity have been detected
-
+		// Find the socket where activity have been detected:
 		// First check for already established connections
 		for(std::list<Client*>::iterator client = clients.begin();
 			client != clients.end();
@@ -91,10 +90,12 @@ int main(int argc, void** argv)
 				}
 			}
 		}
-		// Then check if there is any new incoming connection
+		// First check if there is any new incoming connection
 		if( FD_ISSET(listen_sock.GetFD(), &acting_sock_set) )
 		{
-			clients.push_back( listen_sock.NewConnection() );
+			Client* client = listen_sock.NewConnection();
+			if(client != NULL)
+				clients.push_back( client );
 			DPRINT(CONN, "%i connections up!", clients.size());
 		}
 	}
