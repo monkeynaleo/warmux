20dabeb9d4ca984d9b5f8477b5315605b91329d6
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2006-12-29 19:47:36 +0000

    Messages box now takes care of multilines text !

--- trunk/src/gui/msg_box.cpp
+++ trunk/src/gui/msg_box.cpp
@@ -16,25 +16,74 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  ******************************************************************************
- * Widget
+ * Message Box
  *****************************************************************************/
 
 #include "../tool/rectangle.h"
-#include "box.h"
-#include "label.h"
+#include "widget.h"
 #include "msg_box.h"
 
-MsgBox::MsgBox(int message_nbr, const Rectanglei& rect, Font* _font) :
-   VBox(rect)
+MsgBox::MsgBox(const Rectanglei& rect, Font* _font) :
+   Widget(rect)
 {
   font = _font;
-  while(message_nbr--)
-    AddWidget(new Label("", Rectanglei(0,0,0,0), *font));
+}
+
+void MsgBox::Flush()
+{
+  std::list<Text *>::iterator it ;
+
+  uint y = 5;
+  for (it = messages.begin(); it != messages.end(); it++) 
+    {
+      y += (*it)->GetHeight() + 5;
+
+      while (int(y) > GetSizeY() && !messages.empty()) {
+	Text* tmp = messages.front();
+	y -= tmp->GetHeight() - 5; 
+	delete tmp;
+	messages.pop_front();
+      }
+    }
 }
 
 void MsgBox::NewMessage(const std::string &msg)
 {
-  DelFirstWidget();
-  AddWidget(new Label(msg, Rectanglei(0,0,0,0), *font));
+  messages.push_back(new Text(msg));
+  messages.back()->SetMaxWidth(GetSizeX());
+
+  // Remove old messages if needed
+  Flush();
+
   ForceRedraw();
+}
+
+void MsgBox::Draw(const Point2i &mousePosition, Surface& surf) const
+{
+  // Draw the border
+  surf.BoxColor(*this, defaultOptionColorBox);
+  surf.RectangleColor(*this, defaultOptionColorRect,2);
+
+  // Draw the messages
+  std::list<Text*>::const_iterator it;
+  int x = GetPositionX()+5;
+  int y = GetPositionY()+5;
+  for (it = messages.begin(); it != messages.end(); it++) {
+    (*it)->DrawTopLeft(Point2i(x,y));
+    y += (*it)->GetHeight() + 5;
+  }
+}
+
+void MsgBox::SetSizePosition(const Rectanglei &rect)
+{
+  StdSetSizePosition(rect);
+
+  // render the messages with the correct width
+  std::list<Text*>::iterator it;
+  for (it = messages.begin(); it != messages.end(); it++) {
+    (*it)->SetMaxWidth(GetSizeX());
+  }
+  
+  // Remove old messages if needed
+  Flush();
 }
