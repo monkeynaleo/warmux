d5b63750eeb417009a2afd927b7edc7594b48f88
drayan <drayan@30ef787d-52ff-0310-b286-e08351bb7647>
2007-02-23 23:20:10 +0000

    Correction of the GetBezierInterpolation method. Change the DrawPolygon method in Surface class. Still the bug when drawing a polygon with texture, if no previous background was present, SDL do not draw anything at all.

--- src/graphic/surface.cpp
+++ src/graphic/surface.cpp
@@ -464,13 +464,13 @@ int Surface::DrawPolygon(const Polygon &poly)
   PolygonBuffer * tmp;
   int result = 0;
   tmp = poly.GetPolygonBuffer();
-  if(poly.IsTextured()) {
-    //result |= filledPolygonRGBA(surface, tmp->vx, tmp->vy, tmp->GetSize(), 0, 0, 0, 255);
-    result |= texturedPolygon(surface, tmp->vx, tmp->vy, tmp->GetSize(), poly.GetTexture()->surface, 0, 0);
-  } else if(poly.IsPlaneColor()) {
+  if(poly.IsPlaneColor()) {
     Color plane_color = poly.GetPlaneColor();
     result |= filledPolygonRGBA(surface, tmp->vx, tmp->vy, tmp->GetSize(), plane_color.GetRed(),
                                 plane_color.GetGreen(), plane_color.GetBlue(), plane_color.GetAlpha());
+  }
+  if(poly.IsTextured()) {
+    result |= texturedPolygon(surface, tmp->vx, tmp->vy, tmp->GetSize(), poly.GetTexture()->surface, 0, 0);
   }
   if(poly.IsBordered()) {
     Color border_color = poly.GetBorderColor();
