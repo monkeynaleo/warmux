227d2b221897ce56b7502b2410397b89ba8f7b09
lodesi <lodesi@30ef787d-52ff-0310-b286-e08351bb7647>
2007-03-31 18:52:45 +0000

    bugfix: quit the game when everyone disconnected (without breaking the pause men)

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -145,18 +145,14 @@ void Game::Start()
       MSG_DEBUG( "game", "End of game_loop.Run()" );
       isGameLaunched = false;
 
-      //This commented code is apparently here for some reason, and has been for a while,
-      //however, it breaks the pause menu. It didn't always though...
-      //if (want_end_of_game)
-      //{
-        if (Time::GetInstance()->IsGamePaused()){
-          DisplayPause();
-        } else {
+      if (Time::GetInstance()->IsGamePaused()){
+        DisplayPause();
+      } else {
+        if (want_end_of_game)
           end = DisplayQuit();
-        }
-      //}
-      //else
-      //  end = true;
+        else
+          end = true;
+      }
 
       if (!end)
       {
