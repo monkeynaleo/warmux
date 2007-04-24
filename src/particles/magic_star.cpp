03c83700f1e5d40adfdfb2feb84d7b705e66d8bc
lodesi <lodesi@30ef787d-52ff-0310-b286-e08351bb7647>
2007-04-24 20:32:41 +0000

    Update copyright

--- src/particles/magic_star.cpp
+++ src/particles/magic_star.cpp
@@ -1,7 +1,7 @@
 
 /******************************************************************************
  *  Wormux is a convivial mass murder game.
- *  Copyright (C) 2001-2004 Lawrence Azzoug.
+ *  Copyright (C) 2001-2007 Wormux Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
