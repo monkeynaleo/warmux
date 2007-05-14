bf6a3874ec245a9d275e26eb27b7db11eb144636
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2007-05-14 22:09:07 +0000

    [MENU] Add some messages in server side network menu when a client has confirmed its team choice

--- trunk/src/menu/network_menu.h
+++ trunk/src/menu/network_menu.h
@@ -46,7 +46,7 @@ class NetworkMenu : public Menu
   Box* options_box;
   Label* mode;
   Label* connected_players;
-  Label* inited_players;
+  Label* initialized_players;
 
   /* Chat controllers */
   MsgBox* msg_box;
