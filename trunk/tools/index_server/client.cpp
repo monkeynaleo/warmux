26e0a2cb04db55bbf00ce57eb9a54d976f7ac659
lodesi <lodesi@30ef787d-52ff-0310-b286-e08351bb7647>
2007-08-08 21:04:26 +0000

    Allow beta2 to connect

--- trunk/tools/index_server/client.cpp
+++ trunk/tools/index_server/client.cpp
@@ -72,6 +72,7 @@ bool Client::HandleMsg(const std::string & str)
 			return false;
 		version = str;
 		if(str == "0.8beta1"
+		|| str == "0.8beta2"
 		|| str == "0.7.9rc1"
 		|| str == "0.7.9rc2"
                 || str == "0.7.9")
