7001441b9314a210bbc9f256772e709cea33fdc2
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2007-08-09 23:02:53 +0000

    Remove warning messages from Network connection menu

--- trunk/src/menu/network_connection_menu.cpp
+++ trunk/src/menu/network_connection_menu.cpp
@@ -96,10 +96,6 @@ NetworkConnectionMenu::NetworkConnectionMenu() :
                        Font::FONT_SMALL, Font::FONT_NORMAL);
   widgets.AddWidget(msg_box);
 
-  msg_box->NewMessage(_("WARNING!! Network is still under developement and therefore a little experimental."), c_red);
-  msg_box->NewMessage(""); // Skip a line
-  msg_box->NewMessage(_("Some weapons are disabled, because of known bugs (grapple, airhammer, blowtorch, submachine gun) and surely many other things don't work either!"));
-  msg_box->NewMessage(""); // Skip a line
   msg_box->NewMessage(_("Join #wormux on irc.freenode.net to find some opponents."));
   msg_box->NewMessage(""); // Skip a line
   msg_box->NewMessage(_("Have a good game!"));
