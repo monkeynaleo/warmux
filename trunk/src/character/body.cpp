abc32c4664419cd7a118c0a79594806036543f03
lodesi <lodesi@30ef787d-52ff-0310-b286-e08351bb7647>
2007-10-03 19:41:44 +0000

    Fix previous commit

--- trunk/src/character/body.cpp
+++ trunk/src/character/body.cpp
@@ -131,13 +131,6 @@ Body::Body(xmlpp::Element* xml, const Profile* res):
     else
       mvt_lst[name] = mvt;
 
-    if((mvt_lst.find("black") == mvt_lst.end() && clothes_lst.find("black") != clothes_lst.end())
-    || (mvt_lst.find("black") != mvt_lst.end() && clothes_lst.find("black") == clothes_lst.end()))
-    {
-      std::cerr << "Error: The movement \"black\" or the clothe \"black\" is not defined!" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-
 
     for(std::map<std::string, std::string>::iterator it = mvt_alias.begin();
         it != mvt_alias.end();  ++it)
@@ -149,6 +142,14 @@ Body::Body(xmlpp::Element* xml, const Profile* res):
     }
     it3++;
   }
+
+  if((mvt_lst.find("black") == mvt_lst.end() && clothes_lst.find("black") != clothes_lst.end())
+  || (mvt_lst.find("black") != mvt_lst.end() && clothes_lst.find("black") == clothes_lst.end()))
+  {
+    std::cerr << "Error: The movement \"black\" or the clothe \"black\" is not defined!" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
 }
 
 Body::Body(const Body& _body):
