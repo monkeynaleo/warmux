1421e5907b5a8a9c9c34857cb220b3b2ba1efc24
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2007-11-17 17:32:40 +0000

    [Win32] Increase version number...

--- src/msvc/config.h
+++ src/msvc/config.h
@@ -55,16 +55,16 @@
 #define PACKAGE_NAME "Wormux"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Wormux 0.8beta1"
+#define PACKAGE_STRING "Wormux 0.8beta3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "wormux"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.8beta1"
+#define PACKAGE_VERSION "0.8beta3"
 
 /* ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.8beta1"
+#define VERSION "0.8beta3"
