8a68fb2de76e15bbed977d64c0a0a76aeb34b58a
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2007-11-30 17:57:36 +0000

    Remove wrong and useless code

--- trunk/src/network/network.cpp
+++ trunk/src/network/network.cpp
@@ -143,9 +143,6 @@ int Network::ThreadRun(void*/*no_param*/)
 {
   MSG_DEBUG("network", "Thread created: %u", SDL_ThreadID());
   GetInstance()->ReceiveActions();
-#ifndef WIN32
-  Disconnect(); // this is really needed but it causes a deadlock on WIN32 for unknown reason
-#endif
   std::cout << "Network : end of thread" << std::endl;
   return 1;
 }
