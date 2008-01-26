ebeca32f55110f2918e4296f6b25b9d58404d55f
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2008-01-26 00:16:36 +0000

    New game mode: blitz, like the chess game variation.
    Run wormux with -b option to activate it.
    Not playable over network.
    Use turn time (60s) for all players of a team until finished, so you might want to increase that.
    Refactor Game class a consequence, but GameMode remains unchanged.

--- trunk/src/game/game.cpp
+++ trunk/src/game/game.cpp
@@ -19,12 +19,13 @@
  * Init the game, handle drawing and states of the game.
  *****************************************************************************/
 #include <iostream>
-#include "game/game.h"
-#include "game/config.h"
-#include "game/time.h"
 #include "ai/ai_engine.h"
-#include "map/camera.h"
 #include "character/character.h"
+#include "game/config.h"
+#include "game/game.h"
+#include "game/game_classic.h"
+#include "game/game_blitz.h"
+#include "game/time.h"
 #include "game/game_init.h"
 #include "game/game_mode.h"
 #include "graphic/fps.h"
@@ -38,6 +39,7 @@
 #include "interface/joystick.h"
 #include "interface/mouse.h"
 #include "interface/game_msg.h"
+#include "map/camera.h"
 #include "map/map.h"
 #include "map/maps_list.h"
 #include "map/wind.h"
@@ -65,12 +67,23 @@
 #endif
 
 
-Game * Game::singleton = NULL;
+Game * Game::singleton       = NULL;
+Game::game_mode_t Game::mode = CLASSIC;
 
 Game * Game::GetInstance()
 {
   if (singleton == NULL) {
-    singleton = new Game();
+    switch (mode) {
+    case CLASSIC:
+      singleton = new GameClassic();
+      break;
+    case BLITZ:
+      singleton = new GameBlitz();
+      break;
+    default:
+      fprintf(stderr, "Non-classic game not implemented\n");
+      exit(1);
+    }
   }
   return singleton;
 }
@@ -157,13 +170,12 @@ std::string Game::GetUniqueId()
 
 
 Game::Game():
-  isGameLaunched(false),
-  ask_for_menu(false),
   state(PLAYING),
-  pause_seconde(0),
-  duration(0),
-  current_ObjBox(NULL),
   give_objbox(true),
+  pause_seconde(0),
+  isGameLaunched(false),
+  current_ObjBox(NULL),
+  ask_for_menu(false),
   fps(new FramePerSecond()),
   delay(0),
   time_of_next_frame(0),
@@ -417,18 +429,6 @@ void Game::MessageEndOfGame() const
 }
 
 
-void Game::EndOfGame()
-{
-  Network::GetInstance()->SetTurnMaster(true);
-  SetState(END_TURN);
-  duration = GameMode::GetInstance()->duration_exchange_player + 2;
-  GameMessages::GetInstance()->Add (_("And the winner is..."));
-
-  while (duration >= 1 ) {
-    MainLoop();
-  }
-}
-
 void Game::MainLoop()
 {
   // Refresh clock value
@@ -468,72 +468,6 @@ void Game::MainLoop()
   delay = time_of_next_phy_frame - Time::GetInstance()->ReadRealTime();
   if (delay >= 0)
     SDL_Delay(delay);
-}
-
-void Game::RefreshClock()
-{
-  Time * global_time = Time::GetInstance();
-  if (global_time->IsGamePaused()) return;
-  global_time->Refresh();
-
-  if (1000 < global_time->Read() - pause_seconde)
-    {
-      pause_seconde = global_time->Read();
-
-      switch (state) {
-
-      case PLAYING:
-        if (duration <= 1) {
-	  jukebox.Play("share", "end_turn");
-	  SetState(END_TURN);
-        } else {
-          duration--;
-          Interface::GetInstance()->UpdateTimer(duration);
-	  if (duration <= 5) {
-	    jukebox.Play("share", "time/bip");
-	  }
-        }
-        break;
-
-      case HAS_PLAYED:
-        if (duration <= 1) {
-          SetState(END_TURN);
-        } else {
-          duration--;
-          Interface::GetInstance()->UpdateTimer(duration);
-        }
-        break;
-
-      case END_TURN:
-        if (duration <= 1) {
-
-          if (IsAnythingMoving()) {
-            duration = 1;
-            // Hack to be sure that nothing is moving since enough time
-            // it avoids giving hand to another team during the end of an explosion for example
-            break;
-          }
-
-          if (IsGameFinished()) {
-            duration--;
-            break;
-          }
-
-          if (Network::GetInstance()->IsTurnMaster() && give_objbox && world.IsOpen()) {
-            NewBox();
-            give_objbox = false;
-            break;
-          }
-          else {
-            SetState(PLAYING);
-            break;
-          }
-        } else {
-          duration--;
-        }
-        break;
-      } // switch
-    }// if
 }
 
 bool Game::NewBox()
@@ -578,11 +512,6 @@ bool Game::NewBox()
   return false;
 }
 
-uint Game::GetRemainingTime() const
-{
-  return duration;
-}
-
 void Game::AddNewBox(ObjBox * box)
 {
   lst_objects.AddObject(box);
@@ -591,89 +520,6 @@ void Game::AddNewBox(ObjBox * box)
   SetCurrentBox(box);
 }
 
-// Begining of a new turn
-void Game::__SetState_PLAYING()
-{
-  MSG_DEBUG("game.statechange", "Playing" );
-
-  // initialize counter
-  duration = GameMode::GetInstance()->duration_turn;
-  Interface::GetInstance()->UpdateTimer(duration);
-  Interface::GetInstance()->EnableDisplayTimer(true);
-  pause_seconde = Time::GetInstance()->Read();
-
-  if (Network::GetInstance()->IsTurnMaster() || Network::GetInstance()->IsLocal())
-    wind.ChooseRandomVal();
-
-  character_already_chosen = false;
-
-  // Prepare each character for a new turn
-  FOR_ALL_LIVING_CHARACTERS(team,character)
-    character->PrepareTurn();
-
-  // Select the next team
-  ASSERT (!IsGameFinished());
-
-  if (Network::GetInstance()->IsTurnMaster() || Network::GetInstance()->IsLocal())
-    {
-      GetTeamsList().NextTeam();
-
-      if ( GameMode::GetInstance()->allow_character_selection==GameMode::CHANGE_ON_END_TURN
-           || GameMode::GetInstance()->allow_character_selection==GameMode::BEFORE_FIRST_ACTION_AND_END_TURN)
-        {
-          ActiveTeam().NextCharacter();
-        }
-
-      Camera::GetInstance()->FollowObject (&ActiveCharacter(), true);
-
-      if ( Network::GetInstance()->IsTurnMaster() )
-        {
-          // Tell to clients which character in the team is now playing
-          Action playing_char(Action::ACTION_GAMELOOP_CHANGE_CHARACTER);
-          playing_char.StoreActiveCharacter();
-          Network::GetInstance()->SendAction(&playing_char);
-
-          printf("Action_ChangeCharacter:\n");
-          printf("char_index = %i\n",ActiveCharacter().GetCharacterIndex());
-          printf("Playing character : %i %s\n", ActiveCharacter().GetCharacterIndex(), ActiveCharacter().GetName().c_str());
-          printf("Playing team : %i %s\n", ActiveCharacter().GetTeamIndex(), ActiveTeam().GetName().c_str());
-          printf("Alive characters: %i / %i\n\n",ActiveTeam().NbAliveCharacter(),ActiveTeam().GetNbCharacters());
-        }
-
-      // Are we turn master for next turn ?
-      if (ActiveTeam().IsLocal() || ActiveTeam().IsLocalAI())
-        Network::GetInstance()->SetTurnMaster(true);
-      else
-        Network::GetInstance()->SetTurnMaster(false);
-    }
-
-  give_objbox = true; //hack make it so no more than one objbox per turn
-}
-
-void Game::__SetState_HAS_PLAYED()
-{
-  MSG_DEBUG("game.statechange", "Has played, now can move");
-  duration = GameMode::GetInstance()->duration_move_player;
-  pause_seconde = Time::GetInstance()->Read();
-  Interface::GetInstance()->UpdateTimer(duration);
-  CharacterCursor::GetInstance()->Hide();
-}
-
-void Game::__SetState_END_TURN()
-{
-  MSG_DEBUG("game.statechange", "End of turn");
-  ActiveTeam().AccessWeapon().SignalTurnEnd();
-  ActiveTeam().AccessWeapon().Deselect();
-  CharacterCursor::GetInstance()->Hide();
-  duration = GameMode::GetInstance()->duration_exchange_player;
-  Interface::GetInstance()->UpdateTimer(duration);
-  Interface::GetInstance()->EnableDisplayTimer(false);
-  pause_seconde = Time::GetInstance()->Read();
-
-  // Applying Disease damage and Death mode.
-  ApplyDiseaseDamage();
-  ApplyDeathMode();
-}
 
 void Game::Really_SetState(game_loop_state_t new_state)
 {
@@ -838,31 +684,6 @@ void Game::ApplyDiseaseDamage() const
       character->DecDiseaseDuration();
     }
   }
-}
-
-// Reduce energy of each character if we are in death mode
-void Game::ApplyDeathMode () const
-{
-  if(Time::GetInstance()->Read() > GameMode::GetInstance()->duration_before_death_mode * 1000)
-  {
-    GameMessages::GetInstance()->Add (_("Hurry up, you are too slow !!"));
-    FOR_ALL_LIVING_CHARACTERS(team, character)
-    {
-      // If the character energy is lower than damage
-      // per turn we reduce the character's health to 1
-      if (static_cast<uint>(character->GetEnergy()) >
-          GameMode::GetInstance()->damage_per_turn_during_death_mode)
-        // Don't report damage to the active character, it's not the responsible for this damage
-        character->SetEnergyDelta(-(int)GameMode::GetInstance()->damage_per_turn_during_death_mode, false);
-      else
-        character->SetEnergy(1);
-    }
-  }
-}
-
-bool Game::IsGameFinished() const
-{
-  return (NbrRemainingTeams() <= 1);
 }
 
 int Game::NbrRemainingTeams() const
