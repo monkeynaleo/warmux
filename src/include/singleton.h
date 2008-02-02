5d4895dcd007d69532441ec2b443eb71fd426c8a
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2008-02-02 18:59:11 +0000

    Check that the instance being destroyed is really the singleton. Also remove duplicated code...

--- src/include/singleton.h
+++ src/include/singleton.h
@@ -22,6 +22,7 @@
 #ifndef SINGLETON_H
 #define SINGLETON_H
 
+#include <typeinfo>
 #include <list>
 
 // Forward declarations
@@ -45,76 +46,16 @@ class Singleton : public BaseSingleton
 {
 protected:
   static T* singleton;
-  ~Singleton() { singleton = NULL; }
-public:
-  static T* GetInstance()
-  {
-    if (!singleton) singleton = new T();
-    return singleton;
-  }
-  static const T* GetConstInstance() { return GetInstance(); }
+  ~Singleton() { if (singleton == this) singleton = NULL; }
 
-  static T& GetRef() { return *GetInstance(); }
-  static const T& GetConstRef() { return *GetInstance(); }
-
-  static void CleanUp() { if (singleton) delete singleton; }
-};
-
-template<typename T>
-T* Singleton<T>::singleton = NULL;
-
-#endif // SINGLETON_H
-
-#ifndef SINGLETON_H
-#define SINGLETON_H
-
-#include <list>
-#include "tool/debug.h"
-
-class BaseSingleton;
-
-typedef std::list<BaseSingleton*> SingletonList;
-extern SingletonList singletons;
-
-class BaseSingleton
-{
-public:
-  BaseSingleton()
-  {
-    singletons.push_back(this);
-    MSG_DEBUG("singleton", "Added %p\n", this);
-  }
-  virtual ~BaseSingleton()
-  {
-    singletons.remove(this);
-    MSG_DEBUG("singleton", "Removed %p\n", this);
-  }
-  static void ReleaseSingletons()
-  {
-    SingletonList copy(singletons);
-    for (SingletonList::iterator it = copy.begin();
-         it != copy.end();
-         ++it)
-    {
-      MSG_DEBUG("singleton", "Releasing %p\n", *it);
-      delete (*it);
-    }
-  };
-};
-
-template<typename T>
-class Singleton : public BaseSingleton
-{
-protected:
-  static T* singleton;
-  ~Singleton() { singleton = NULL; }
 public:
   static T* GetInstance()
   {
     if (!singleton)
     {
       singleton = new T();
-      MSG_DEBUG("singleton", "Singleton is %p\n", singleton);
+      MSG_DEBUG("singleton", "Created singleton %p of type %s\n",
+                singleton, typeid(singleton).name());
     }
     return singleton;
   }
