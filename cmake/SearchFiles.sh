7e51c4575409cd4f837e0c1e365877a37241ee17
lynxlynxlynx <lynxlynxlynx@30ef787d-52ff-0310-b286-e08351bb7647>
2008-04-23 21:42:31 +0000

    cmake: use glob patterns instead of hardcoding filenames no more adding files on each update! This is now only needed for data. TODO: fix that too

--- cmake/SearchFiles.sh
+++ cmake/SearchFiles.sh
@@ -10,11 +10,3 @@ SET(DATA_FILES
 `find ../data/ -type f | grep -v -e "\.svn\|Makefile\|~" | cut -c 9- | sort`
 )" > InstallData.cmake
 
-# Search .cpp source files
-
-cd ../src
-echo "
-SET(WORMUX_SRCS
-`find . -name '*.cpp' -o -name '*.c' | cut -c 3- | sort`
-)" > SourceFiles.cmake
-
