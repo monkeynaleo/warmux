eba7efddfe2dfd0085704d99f3e09642a965ee4b
lynxlynxlynx <lynxlynxlynx@30ef787d-52ff-0310-b286-e08351bb7647>
2008-05-13 16:50:13 +0000

    fix polecat not farting (by ivze) virtual method was defined, not redefined

--- src/particles/polecat_fart.h
+++ src/particles/polecat_fart.h
@@ -30,7 +30,7 @@ class PolecatFart : public Particle
   public:
     PolecatFart();
   protected:
-    void SignalObjectCollision(PhysicalObj * obj);
+    void SignalObjectCollision(PhysicalObj * obj, const Point2d& my_speed_before);
 };
 
 #endif /* POLECAT_FART_H */
