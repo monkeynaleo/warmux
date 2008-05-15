bd8cc20171703b1c171f561f4c56db8efa0cdd90
plorf <plorf@30ef787d-52ff-0310-b286-e08351bb7647>
2008-05-15 08:09:00 +0000

    mac : update cmake_build.sh : auria check for frameworks

--- branches/0.8-final/mac/cmake_build.sh
+++ branches/0.8-final/mac/cmake_build.sh
@@ -13,14 +13,26 @@ mkdir -p ../Wormux.app/Contents/Resources
 mkdir -p ../Wormux.app/Contents/MacOS
 mkdir -p ../Wormux.app/Contents/Frameworks
 
+echo "Copy data into .app file"
+
+# Copy data files into .app
+mkdir -p ../Wormux.app/Contents/Resources/data
+echo "Error : i think to much data choose.. (140MB at end)"
+cp -R ../../data/ ../Wormux.app/Contents/Resources/data/
+
+# Copy locale files into .app
+mkdir -p ../Wormux.app/Contents/Resources/locale
+echo "Error : wormux in english, so i think it's not working... search where is it"
+cp -R /usr/share/locale ../Wormux.app/Contents/Resources/locale
+
 
 # pwd = /mac
 cd ..
 
-echo "Copy data into .app file"
-echo "Add icon and info.plist"
+# Add icon and info.plist and PkgInfo
 cp Info.plist.in ./Wormux.app/Contents/info.plist
-cp ../data/wormux_128x128.icns ./Wormux.app/Contents/Resources/wormux.icns
+cp PkgInfo.in ./Wormux.app/Contents/
+cp ../data/wormux_128x128.icns ./Wormux.app/Contents/Resources/Wormux.icns
 
 
 #pwd = /mac/tmpbuild
@@ -35,6 +47,10 @@ cp -r /usr/local/share/locale ../Wormux.app/Contents/Resources/locale
 
 #pwd = /mac
 cd ..
+
+echo "COPY DYLIB OR FRAMEWORKS ????"
+echo "Check if write rights are ok to copy frameworks"
+echo "Copy all frameworks"
 
 echo "Remove temps files"
 #rm -rf tmpbuild
