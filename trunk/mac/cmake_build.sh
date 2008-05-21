b204c58c0c23ac78d04cd34a49322030e5d6492f
plorf <plorf@30ef787d-52ff-0310-b286-e08351bb7647>
2008-05-21 21:33:27 +0000

    mac : cmake_build update

--- trunk/mac/cmake_build.sh
+++ trunk/mac/cmake_build.sh
@@ -25,10 +25,11 @@ export CMAKE_INSTALL_PREFIX=./wormux-files
 cd ${TMP}
 
 awk '/^SET\(WORMUX_PATCH/ { sub(/^/,"#") } { print }' ${ROOT}CMakeLists.txt > tmp.$$.$$
-cp CMakeLists.txt tmp.$$.$$.2
-mv tmp.$$.$$ {ROOT}CMakeLists.txt
-cmake ../.. -DPREFIX:PATCH=${TMPDATA}
-mv tmp.$$.$$.2 {ROOT}CMakeLists.txt
+cp ${ROOT}CMakeLists.txt tmp.$$.$$.2
+mv tmp.$$.$$ ${ROOT}CMakeLists.txt
+#cmake ../.. -DPREFIX:PATCH=${TMPDATA}
+cmake ../..
+mv tmp.$$.$$.2 ${ROOT}CMakeLists.txt
 
 make
 make install
@@ -50,26 +51,37 @@ APP=${MAC}Wormux.app/
 rm -rf ${APP} ${ARCHIVE}
 
 mkdir -p ${APP}
-mkdir -p ${APP}Contents/Resources
-mkdir -p ${APP}Contents/MacOS
-mkdir -p ${APP}Contents/Frameworks
+mkdir -p ${APP}Contents/Resources/
+mkdir -p ${APP}Contents/MacOS/
+mkdir -p ${APP}Contents/Frameworks/
 RES=${APP}Contents/Resources/
-mkdir -p ${RES}data
-mkdir -p ${RES}locale
+mkdir -p ${RES}data/
+mkdir -p ${RES}locale/
 
 echo "Copy data into .app file"
 
 # Copy data files into .app
 echo "Copy /data"
-cp -R ${TMPDATA}share/wormux/* ${RES}data/
-
+#cp -R ${TMPDATA}share/wormux/* ${RES}data/
 INSTALL=/usr/local/share/
-#cp -R ${INSTALL}wormux ${RES}data
+cp -R ${INSTALL}wormux/* ${RES}data
 
 # Copy locale files into .app
 #echo "Error : wormux in english, so i think it's not working... search where is it"
-cp -R ${INSTALL}locale ${RES}/locale
+#cp -R ${INSTALL}locale ${RES}locale
 
+
+# Install the locale files
+#echo "Copying locale files to           ${RES}/locale"
+#make -w -C ${ROOT}/po -e all-local-yes DESTDIR=${RES}
+#make -w -C ${ROOT}/po -e install-data-local-yes localedir=${RES}/locale
+
+TMPLOCALE=;
+for TMPLOCALE in /usr/share/locale/??;
+do [[ -e $lang/LC_MESSAGES/wormux.mo ]] || continue;
+    mkdir -p $target/$lang/LC_MESSAGES;
+    cp $lang/LC_MESSAGES/wormux.mo $target/$lang/LC_MESSAGES/wormux.mo;
+done
 
 # Add icon and info.plist and PkgInfo
 cp ${MAC}Info.plist.in ${APP}Contents/info.plist
@@ -78,8 +90,8 @@ cp ${ROOT}data/wormux_128x128.icns ${RES}Wormux.icns
 
 
 echo "Add bin"
-#cp ${TMP}src/wormux ${APP}Contents/MacOS/wormux
-cp ${TMPDATA}bin/wormux ${APP}Contents/MacOs/wormux
+cp ${TMP}src/wormux ${APP}Contents/MacOS/wormux
+#cp ${TMPDATA}bin/wormux ${APP}Contents/MacOs/wormux
 
 echo "Copy all frameworks"
 cd ${MAC};
