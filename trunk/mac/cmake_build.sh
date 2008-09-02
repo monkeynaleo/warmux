1f3bda4de8f6df6ff40f99c28893ac5ba0e254a9
plorf <plorf@30ef787d-52ff-0310-b286-e08351bb7647>
2008-09-02 14:06:39 +0000

    mac : Update files for using new release number 0.8.1svn instead of 0.8svn

--- trunk/mac/cmake_build.sh
+++ trunk/mac/cmake_build.sh
@@ -26,7 +26,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.4
 export FAT_CFLAGS="-isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch ppc -arch i386 -I/Developer/SDKs/MacOSX10.4u.sdk/usr/include"
 export FAT_LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -arch ppc -arch i386 -L/Developer/SDKs/MacOSX10.4u.sdk/usr/lib"
 
-APP_VERSION=0.8svn
+APP_VERSION=0.8.1svn
 BUNDLE_NAME=Wormux
 DMG_TARGET="${BUNDLE_NAME}-${APP_VERSION}"
 DMG_OUT=${BUNDLE_NAME}-${APP_VERSION}-`uname -p`
@@ -166,14 +166,14 @@ cp ${ROOT}data/wormux_128x128.icns ${RES}Wormux.icns
 
 
 
-ARCHIVE=${MAC}Wormux-0.8svn.tar.bz2
-if [ -e ${ARCHIVE} ]
-then
-    echo "******************"
-    echo "Remove old archive" 
-    rm -f ${ARCHIVE}
-    echo "******************"
-fi
+#ARCHIVE=${MAC}Wormux-0.8.1svn.tar.bz2
+#if [ -e ${ARCHIVE} ]
+#then
+#    echo "******************"
+#    echo "Remove old archive" 
+#    rm -f ${ARCHIVE}
+#    echo "******************"
+#fi
 
 #export CMAKE_INSTALL_PREFIX=${RES}
 
