4f842cc4b10895aec8ce6c54a27eaa60765e9ec1
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2008-09-23 16:45:32 +0000

    Re-enable cmaera following object
    There was a problem with PhysicalObj::FootsInVacuum returning always true when just on top of a character or a barrel because for an object, its
    Box2D shape was different than its Wormux test rectangle.

--- src/map/camera.cpp
+++ src/map/camera.cpp
@@ -312,17 +312,18 @@ void Camera::Refresh(){
     AutoCrop();
 }
 
-void Camera::FollowObject(const PhysicalObj */*obj*/, bool /*follow*/,
-			  bool /*_in_advance*/)
+void Camera::FollowObject(const PhysicalObj *obj, bool follow,
+			  bool _in_advance)
 {
-  /*MSG_DEBUG( "camera.tracking", "Following object %s",
-    obj->GetName().c_str());*/
- // Mouse::GetInstance()->Hide();
- // if (followed_object != obj || !IsVisible(*obj) || auto_crop != follow)
- //   auto_crop = follow;
+  MSG_DEBUG( "camera.tracking", "Following object %s", obj->GetName().c_str());
 
- // in_advance = _in_advance;
- // followed_object = obj;
+  Mouse::GetInstance()->Hide();
+
+  if (followed_object != obj || !IsVisible(*obj) || auto_crop != follow)
+    auto_crop = follow;
+
+  in_advance = _in_advance;
+  followed_object = obj;
 }
 
 void Camera::StopFollowingObj(const PhysicalObj* obj){
@@ -348,7 +349,7 @@ Point2i Camera::ComputeShake() const
     if ( time > m_started_shaking + m_shake_duration || m_shake_duration == 0 )
     {
         return Point2i( 0, 0 ); // not shaking now
-    };
+    }
 
     if ( time == m_last_time_shake_calculated )
         return m_shake;
@@ -363,7 +364,7 @@ Point2i Camera::ComputeShake() const
         float arg = k_scale_angle * t;
         // denormalized sinc
         func_val = ( 1 - t ) * sin( arg ) / arg;
-    };
+    }
 
     float x_ampl = ( float )RandomLocal().GetDouble( -m_shake_amplitude.x, m_shake_amplitude.x );
     float y_ampl = ( float )RandomLocal().GetDouble( -m_shake_amplitude.y, m_shake_amplitude.y );
@@ -380,11 +381,11 @@ Point2i Camera::ComputeShake() const
             t, func_val, m_shake.x, m_shake.y );
 
         t_last_time_logged = time;
-    };
+    }
 
     m_last_time_shake_calculated = time;
     return m_shake;
-};
+}
 
 void Camera::Shake( uint how_long_msec, const Point2i & amplitude, const Point2i & centerpoint )
 {
@@ -409,8 +410,8 @@ void Camera::Shake( uint how_long_msec, const Point2i & amplitude, const Point2i
         m_shake_duration = how_long_msec;
         m_shake_amplitude = amplitude;
         m_shake_centerpoint = centerpoint;
-    };
-};
+    }
+}
 
 void Camera::ResetShake()
 {
@@ -418,4 +419,4 @@ void Camera::ResetShake()
     m_shake_duration = 0;
     m_last_time_shake_calculated = 0;
     m_shake = Point2i( 0, 0 );
-};
+}
