9d2de7ee04654adfc06981a8881ddf01e4bdf0b7
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2008-11-11 10:37:24 +0000

    [MSVC] Update config.h to follow curl/fribidi and other variables updates.

--- trunk/src/msvc/config.h
+++ trunk/src/msvc/config.h
@@ -1,6 +1,7 @@
 /* src/msvc/config.h copied from a MINGW-generated src/config.h  */
 
-/* Translation of program messages to the user's native language is requested. */
+/* Define to 1 if translation of program messages to the user's native
+   language is requested. */
 #define ENABLE_NLS 1
 
 /* GNU dcgettext() function is already present or preinstalled. */
@@ -12,6 +13,9 @@
 /* iconv() function. */
 #include <iconv.h>
 #define HAVE_ICONV 1
+
+/* Define to 1 if you have a functional curl library. */
+#define HAVE_LIBCURL 1
 
 /* `SDL_gfx' library (-lSDL_gfx). */
 #define HAVE_LIBSDL_GFX 1
@@ -46,6 +50,60 @@
 /* <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
+/* Manual addition */
+#define CURL_STATICLIB 1
+
+/* Defined if libcurl supports AsynchDNS */
+/* #undef LIBCURL_FEATURE_ASYNCHDNS */
+
+/* Defined if libcurl supports IDN */
+/* #undef LIBCURL_FEATURE_IDN */
+
+/* Defined if libcurl supports IPv6 */
+/* #undef LIBCURL_FEATURE_IPV6 */
+
+/* Defined if libcurl supports KRB4 */
+/* #undef LIBCURL_FEATURE_KRB4 */
+
+/* Defined if libcurl supports libz */
+#define LIBCURL_FEATURE_LIBZ 1
+
+/* Defined if libcurl supports NTLM */
+/* #undef LIBCURL_FEATURE_NTLM */
+
+/* Defined if libcurl supports SSL */
+/* #undef LIBCURL_FEATURE_SSL */
+
+/* Defined if libcurl supports SSPI */
+/* #undef LIBCURL_FEATURE_SSPI */
+
+/* Defined if libcurl supports DICT */
+#define LIBCURL_PROTOCOL_DICT 1
+
+/* Defined if libcurl supports FILE */
+#define LIBCURL_PROTOCOL_FILE 1
+
+/* Defined if libcurl supports FTP */
+#define LIBCURL_PROTOCOL_FTP 1
+
+/* Defined if libcurl supports FTPS */
+/* #undef LIBCURL_PROTOCOL_FTPS */
+
+/* Defined if libcurl supports HTTP */
+#define LIBCURL_PROTOCOL_HTTP 1
+
+/* Defined if libcurl supports HTTPS */
+/* #undef LIBCURL_PROTOCOL_HTTPS */
+
+/* Defined if libcurl supports LDAP */
+#define LIBCURL_PROTOCOL_LDAP 1
+
+/* Defined if libcurl supports TELNET */
+#define LIBCURL_PROTOCOL_TELNET 1
+
+/* Defined if libcurl supports TFTP */
+#define LIBCURL_PROTOCOL_TFTP 1
+
 /* Name of package */
 #define PACKAGE "wormux"
 
@@ -56,16 +114,19 @@
 #define PACKAGE_NAME "Wormux"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Wormux 0.8svn"
+#define PACKAGE_STRING "Wormux 0.8.1svn"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "wormux"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.8svn"
+#define PACKAGE_VERSION "0.8.1svn"
 
 /* ANSI C header files. */
 #define STDC_HEADERS 1
 
+/* Define to use FriBidi */
+#define USE_FRIBIDI 1
+
 /* Version number of package */
-#define VERSION "0.8svn"
+#define VERSION "0.8.1svn"
