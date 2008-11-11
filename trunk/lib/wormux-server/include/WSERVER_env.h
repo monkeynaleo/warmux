a2e18d97e980a36524558c81cb1d1aac60819863
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2008-11-11 20:11:14 +0000

    Fix copyright

--- trunk/lib/wormux-server/include/WSERVER_env.h
+++ trunk/lib/wormux-server/include/WSERVER_env.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *  Wormux is a convivial mass murder game.
- *  Copyright (C) 2001-2004 Lawrence Azzoug.
+ *  Copyright (C) 2001-2008 Wormux Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
