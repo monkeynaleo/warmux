fb4382edbc4ab770ee9c427543ad0a8f77e14600
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2008-12-26 21:46:48 +0000

    Network statistics: allow to set the begin and end dates for daily stats

--- website/php/net_daily.php
+++ website/php/net_daily.php
@@ -2,6 +2,20 @@
 
 include "./net_stats_img.php";
 
+// =========================================================
+
+$begindate = "";
+if (array_key_exists("begin", $_GET)) {
+  $begindate = $_GET['begin'];
+}
+
+$enddate="";
+if (array_key_exists("end", $_GET)) {
+  $enddate = $_GET['end'];
+}
+
+// =========================================================
+
 $days = array();
 $servers = array();
 $players = array();
@@ -10,6 +24,9 @@ function generate_stats() {
   $stats = "./net_stats/daily.csv";
   $fh = fopen($stats, 'r');
 
+  $beginline = split("-", $GLOBALS['begindate']);
+  $endline = split("-", $GLOBALS['enddate']);
+
   $i = 0;
   while (!feof($fh)) {
     $line = fgets($fh, 4096);
@@ -17,13 +34,44 @@ function generate_stats() {
     $arrayline = split(" ", $line);
 
     if (count($arrayline) == 5) {
+
+      $date = $arrayline[0];
+      $dateline = split("-", $date);
+
+      if (count($beginline) == 3) {
+	if ($dateline[0] < $beginline[0]) // year
+	  continue;
+
+	if ($dateline[0] == $beginline[0]
+	    && $dateline[1] < $beginline[1]) // month
+	  continue;
+
+	if ($dateline[0] == $beginline[0]
+	    && $dateline[1] == $beginline[1]
+	    && $dateline[2] < $beginline[2]) // day
+	  continue;
+      }
+
+      if (count($endline) == 3) {
+	if ($dateline[0] > $endline[0]) // year
+	  continue;
+
+	if ($dateline[0] == $endline[0]
+	    && $dateline[1] > $endline[1]) // month
+	  continue;
+
+	if ($dateline[0] == $endline[0]
+	    && $dateline[1] == $endline[1]
+	    && $dateline[2] > $endline[2])
+	  continue;
+      }
+
       $GLOBALS['days'][$i] = $arrayline[0];
       $GLOBALS['servers'][$i] = $arrayline[3];
       $GLOBALS['players'][$i] = $arrayline[4];
       $i++;
     }
   }
-
   fclose($fh);
 }
 
