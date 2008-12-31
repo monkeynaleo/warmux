bfd6de9057e2ec7b21964bc5b3584ee1eb8c42c4
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2008-12-31 15:48:44 +0000

    Network stats web page: Implement display of "disappointed clients" for daily graph
    (was not yet commited even if page was uploaded since one week)
    
    Add some debug functions

--- website/php/net_daily.php
+++ website/php/net_daily.php
@@ -19,6 +19,60 @@ if (array_key_exists("end", $_GET)) {
 $days = array();
 $servers = array();
 $players = array();
+$disappointed_players = array();
+
+function parse_line($i, $line, $beginline, $endline)
+{
+  $arrayline = split(" ", $line);
+
+  if (count($arrayline) >= 5) {
+
+    $date = $arrayline[0];
+    $dateline = split("-", $date);
+
+    if (count($beginline) == 3) {
+      if ($dateline[0] < $beginline[0]) // year
+	return false;
+
+      if ($dateline[0] == $beginline[0]
+	  && $dateline[1] < $beginline[1]) // month
+	return false;
+
+      if ($dateline[0] == $beginline[0]
+	  && $dateline[1] == $beginline[1]
+	  && $dateline[2] < $beginline[2]) // day
+	return false;
+    }
+
+    if (count($endline) == 3) {
+      if ($dateline[0] > $endline[0]) // year
+	return false;
+
+	if ($dateline[0] == $endline[0]
+	    && $dateline[1] > $endline[1]) // month
+	  return false;
+
+	if ($dateline[0] == $endline[0]
+	    && $dateline[1] == $endline[1]
+	    && $dateline[2] > $endline[2])
+	  return false;
+    }
+
+    $GLOBALS['days'][$i] = $arrayline[0];
+    $GLOBALS['servers'][$i] = $arrayline[3];
+    $GLOBALS['players'][$i] = $arrayline[4];
+
+    if (count($arrayline) > 5) {
+      $GLOBALS['disappointed_players'][$i] = $arrayline[5];
+    } else {
+      $GLOBALS['disappointed_players'][$i] = 0;
+    }
+
+    return true;
+  }
+
+  return false;
+}
 
 function generate_stats() {
   $stats = "./net_stats/daily.csv";
@@ -31,46 +85,9 @@ function generate_stats() {
   while (!feof($fh)) {
     $line = fgets($fh, 4096);
     $line = rtrim($line);
-    $arrayline = split(" ", $line);
 
-    if (count($arrayline) == 5) {
-
-      $date = $arrayline[0];
-      $dateline = split("-", $date);
-
-      if (count($beginline) == 3) {
-	if ($dateline[0] < $beginline[0]) // year
-	  continue;
-
-	if ($dateline[0] == $beginline[0]
-	    && $dateline[1] < $beginline[1]) // month
-	  continue;
-
-	if ($dateline[0] == $beginline[0]
-	    && $dateline[1] == $beginline[1]
-	    && $dateline[2] < $beginline[2]) // day
-	  continue;
-      }
-
-      if (count($endline) == 3) {
-	if ($dateline[0] > $endline[0]) // year
-	  continue;
-
-	if ($dateline[0] == $endline[0]
-	    && $dateline[1] > $endline[1]) // month
-	  continue;
-
-	if ($dateline[0] == $endline[0]
-	    && $dateline[1] == $endline[1]
-	    && $dateline[2] > $endline[2])
-	  continue;
-      }
-
-      $GLOBALS['days'][$i] = $arrayline[0];
-      $GLOBALS['servers'][$i] = $arrayline[3];
-      $GLOBALS['players'][$i] = $arrayline[4];
+    if (parse_line($i, $line, $beginline, $endline))
       $i++;
-    }
   }
   fclose($fh);
 }
@@ -79,6 +96,10 @@ generate_stats();
 
 $max = max($players);
 
-draw_graph($days, $players, $servers, $max);
+if (array_key_exists("debug", $_GET)) {
+  print_arrays($players, $servers, $disappointed_players);
+ } else {
+  draw_graph($days, $players, $servers, $disappointed_players, $max);
+ }
 
 ?>
