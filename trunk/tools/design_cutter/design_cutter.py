a278b217d28c72a3b3c18a0e1d11eba9460dab35
fredb219 <fredb219@30ef787d-52ff-0310-b286-e08351bb7647>
2009-02-05 23:52:18 +0000

    add support of rectangular border

--- trunk/tools/design_cutter/design_cutter.py
+++ trunk/tools/design_cutter/design_cutter.py
@@ -4,9 +4,9 @@
 #
 # This program cuts an image in 9 part
 # syntax :
-# design_cutter.py size image
+# design_cutter.py width heigth image
 # example :
-# design_cutter.py 20 big_hide.png
+# design_cutter.py 10 15 big_hide.png
 
 import os, sys
 import Image
@@ -18,13 +18,15 @@ def sub_box(image, left, upper, right, lower):
 i = 0
 for arg in sys.argv:
     if i == 1:
-        size = int(arg)
+        w = int(arg)
     if i == 2:
+        h = int(arg)
+    if i == 3:
         file = arg
     i+=1
 
-if i!=3:
-    print "2 arguments needed"
+if i!=4:
+    print "3 arguments needed"
     exit()
 
 prefix = os.path.splitext(file)[0]
@@ -35,35 +37,35 @@ xsize, ysize = im.size
 try:
 
     #Corner
-    out = sub_box(im, 0, 0, size, size)
+    out = sub_box(im, 0, 0, w, h)
     out.save(prefix+"_top-left.png", "PNG")
     print prefix+"_top-left.png generated"
-    out = sub_box(im, xsize-size, 0, xsize, size)
+    out = sub_box(im, w+1, 0, xsize, h)
     out.save(prefix+"_top-right.png", "PNG")
     print prefix+"_top-left.png generated"
-    out = sub_box(im, 0, ysize-size, size, ysize)
+    out = sub_box(im, 0, h+1, w, ysize)
     out.save(prefix+"_bottom-left.png", "PNG")
     print prefix+"_top-left.png generated"
-    out = sub_box(im, xsize-size, ysize-size, xsize,  ysize)
+    out = sub_box(im, w+1, h+1, xsize,  ysize)
     out.save(prefix+"_bottom-right.png", "PNG")
     print prefix+"_top-left.png generated"
 
     #Border
-    out = sub_box(im, size, 0, xsize-size,  size)
+    out = sub_box(im, w, 0, w+1,  h)
     out.save(prefix+"_top.png", "PNG")
     print prefix+"_top.png generated"
-    out = sub_box(im, size,ysize-size , xsize-size, ysize)
+    out = sub_box(im, w,h+1 , w+1, ysize)
     out.save(prefix+"_bottom.png", "PNG")
     print prefix+"_bottom.png generated"
-    out = sub_box(im, 0, size, size, ysize-size)
+    out = sub_box(im, 0, h, w, h+1)
     out.save(prefix+"_left.png", "PNG")
     print prefix+"_left.png generated"
-    out = sub_box(im, xsize-size, size, xsize,  ysize-size)
+    out = sub_box(im, w+1, h, xsize, h+1)
     out.save(prefix+"_rigth.png", "PNG")
     print prefix+"_right.png generated"
 
     #Center 
-    out = sub_box(im, size, size, xsize-size,  ysize-size)
+    out = sub_box(im,w, h, w+1, h+1)
     out.save(prefix+"_center.png", "PNG")
     print prefix+"_center.png generated"
 
