8603a7a1926501d8c5fb2b25ac00d3bfb06ee8e4
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2009-03-30 20:54:08 +0000

    [bug #10664] fix teleportation when walking with the jetpack

--- branches/wormux-0.8.4/src/weapon/jetpack.h
+++ branches/wormux-0.8.4/src/weapon/jetpack.h
@@ -30,6 +30,8 @@ class JetPack : public Weapon
     double m_x_force;
     double m_y_force;
 
+    bool m_flying;
+
     SoundSample flying_sound;
 
     // Jetpack fuel.
@@ -62,11 +64,8 @@ class JetPack : public Weapon
     void GoUp();
     void GoLeft();
     void GoRight();
-    void StopUp() { m_y_force = 0.0; StopUse(); };
-    void StopLeft() { m_x_force = 0.0; StopUse(); };
-    void StopRight() { m_x_force = 0.0; StopUse(); };
-    void StartUse();
-    void StopUse();
+    void StartFlying();
+    void StopFlying();
 };
 
 #endif /* JETPACK_H */
