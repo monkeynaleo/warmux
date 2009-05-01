6a3532e0019e9b585439b39bf28173376f44d012
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2009-05-01 21:20:47 +0000

    Move class IndexServer to libwormux

--- trunk/src/menu/network_connection_menu.h
+++ trunk/src/menu/network_connection_menu.h
@@ -26,7 +26,7 @@
 #include <list>
 #include "menu.h"
 #include "network/network.h"
-#include "network/index_server.h" // for GameServerInfo
+#include <WORMUX_index_server.h> // for GameServerInfo
 
 // Forward declarations
 class Button;
