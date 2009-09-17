f2dffa9f7adebc5bf073ac0d8dcf024ab1b68c7a
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2009-09-17 17:08:58 +0000

    Move script to update debug masks help from src/ to tools/
    Imported from: svn+ssh://svn.gna.org/svn/wormux/trunk@6628
    Original author: gentildemon
    Original date: Thu Sep 17 17:07:10 2009 +0000

--- branches/wormux-0.8.5/tools/update_debug_masks.sh
+++ branches/wormux-0.8.5/tools/update_debug_masks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sourcefiles=`find -name "*.h" -or -name "*.cpp"`
+sourcefiles=`find src/ -name "*.h" -or -name "*.cpp"`
 if [ "$sourcefiles" = "" ]; then
     echo "error searching the files"
     exit 1
@@ -11,4 +11,4 @@ masks2=`grep -ho 'IsLOGGING[[:space:]]*("[^"]*' $sourcefiles | cut -d\" -f2`
 
 MASKS=`echo $masks1 $masks2 | tr " " "\n" | sort -u`
 MASKS2=`echo $MASKS | sed "s/ / | /g"`
-sed -i "s/std::string used_debug_masks.*/std::string used_debug_masks = \"$MASKS2\";/" include/debugmasks.h
+sed -i "s/std::string used_debug_masks.*/std::string used_debug_masks = \"$MASKS2\";/" src/include/debugmasks.h
