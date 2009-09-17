50d85a176fd57284fc35261ba434bd29b2eb3ac0
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2009-09-17 17:07:07 +0000

    Debug log must be available if WMX_LOG is set, not only if DEBUG is set

--- src/include/debugmasks.h
+++ src/include/debugmasks.h
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  ******************************************************************************/
 
-#ifndef DEBUG
+#ifndef WMX_LOG
 #warning this file must not be included!
 #endif
 
