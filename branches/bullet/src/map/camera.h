b9ffd022e68344c91023d508c7c93196ee354bbc
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2009-09-29 16:59:15 +0000

    #14019 fix camera for hight speed object
    Imported from: svn+ssh://svn.gna.org/svn/wormux/trunk@6375
    Original author: fredb219
    Original date: Mon Aug 17 20:16:54 2009 +0000

--- branches/bullet/src/map/camera.h
+++ branches/bullet/src/map/camera.h
@@ -78,7 +78,7 @@ public:
 
   // Auto crop on an object
   // in_advance is used to center the camera on the direction where the object is going
-  void FollowObject(const GameObj *obj, bool follow, bool in_advance = false);
+  void FollowObject(const GameObj *obj, bool follow, bool in_advance = true);
   void StopFollowingObj(const GameObj* obj);
 
   void CenterOnActiveCharacter();
