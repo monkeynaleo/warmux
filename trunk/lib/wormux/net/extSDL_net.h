a9abcd54790c721381ec32018325d82ddcf34a26
iflo <iflo@30ef787d-52ff-0310-b286-e08351bb7647>
2010-01-26 19:31:39 +0000

    Update the copyright year.

--- trunk/lib/wormux/net/extSDL_net.h
+++ trunk/lib/wormux/net/extSDL_net.h
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /******************************************************************************
  *  Wormux is a convivial mass murder game.
- *  Copyright (C) 2001-2009 Wormux Team.
+ *  Copyright (C) 2001-2010 Wormux Team.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
