852b0175e647e9faaa3f74a6956b0d58a03325aa
lami01 <lami01@30ef787d-52ff-0310-b286-e08351bb7647>
2010-03-18 19:08:58 +0000

    Refactor alignments/spaces in Widget header.

--- src/gui/widget.h
+++ src/gui/widget.h
@@ -38,121 +38,121 @@ struct SDL_keysym;
 
 class Widget : public Rectanglei, public Container
 {
-private:
-  bool has_focus;
-  bool visible;
-  bool is_highlighted;
+  private:
+    bool has_focus;
+    bool visible;
+    bool is_highlighted;
 
-  Color border_color;
-  uint border_size;
-  Color background_color;
-  Color highlight_bg_color;
+    Color border_color;
+    uint border_size;
+    Color background_color;
+    Color highlight_bg_color;
 
-  Color font_color;
-  bool font_shadowed;
-  Font::font_size_t font_size;
-  Font::font_style_t font_style;
+    Color font_color;
+    bool font_shadowed;
+    Font::font_size_t font_size;
+    Font::font_style_t font_style;
 
-  Widget(const Widget&);
-  const Widget& operator=(const Widget&);
+    Widget(const Widget&);
+    const Widget& operator=(const Widget&);
 
- protected:
-  Container * ct;
-  bool need_redrawing;
+  protected:
+    Container * ct;
+    bool need_redrawing;
 
-  // Attributs for XML loading process
-  Profile * profile;
-  const xmlNode * widgetNode;
+    // Attributs for XML loading process
+    Profile * profile;
+    const xmlNode * widgetNode;
 
-  // Attributs used to link a widget with an action
-  std::string actionName;
+    // Attributs used to link a widget with an action
+    std::string actionName;
   
-  Color shadowColor;
+    Color shadowColor;
 
-  virtual void __Update(const Point2i &/* mousePosition */,
-			const Point2i &/* lastMousePosition */) {};
+    virtual void __Update(const Point2i &/* mousePosition */,
+                          const Point2i &/* lastMousePosition */) {};
 
-  void RedrawBackground(const Rectanglei& rect);
+    void RedrawBackground(const Rectanglei & rect);
 
-  // Handle font
-  virtual void OnFontChange() {};
-  const Color& GetFontColor() const { return font_color; };
-  Font::font_size_t GetFontSize() const { return font_size; };
-  Font::font_style_t GetFontStyle() const { return font_style; };
-  bool IsFontShadowed() const { return font_shadowed; };
-  const Color & GetShadowColor(void) const { return this->shadowColor; };
+    // Handle font
+    virtual void OnFontChange() {};
+    const Color & GetFontColor() const { return font_color; };
+    Font::font_size_t GetFontSize() const { return font_size; };
+    Font::font_style_t GetFontStyle() const { return font_style; };
+    bool IsFontShadowed() const { return font_shadowed; };
+    const Color & GetShadowColor(void) const { return this->shadowColor; };
 
-  void ParseXMLMisc(void);
-  void ParseXMLBorder(void);
-  void ParseXMLBackground(void);
-  void ParseXMLPosition(void);
-  void ParseXMLSize(void);
-  int ParseHorizontalTypeAttribut(const std::string & attributName,
+    void ParseXMLMisc(void);
+    void ParseXMLBorder(void);
+    void ParseXMLBackground(void);
+    void ParseXMLPosition(void);
+    void ParseXMLSize(void);
+    int ParseHorizontalTypeAttribut(const std::string & attributName,
+                                    int defaultValue);
+    int ParseVerticalTypeAttribut(const std::string & attributName,
                                   int defaultValue);
-  int ParseVerticalTypeAttribut(const std::string & attributName,
-                                int defaultValue);
 
- public:
-  Widget();
-  Widget(const Point2i &size);
-  Widget(Profile * profile,
-         const xmlNode * widgetNode);
-  virtual ~Widget() { };
+  public:
+    Widget();
+    Widget(const Point2i & size);
+    Widget(Profile * profile,
+           const xmlNode * widgetNode);
+    virtual ~Widget() { };
 
-  virtual bool LoadXMLConfiguration(void) { return false; };
+    virtual bool LoadXMLConfiguration(void) { return false; };
 
-  virtual void Update(const Point2i &mousePosition,
-		      const Point2i &lastMousePosition); // Virtual for widget_list: to remove!
+    virtual void Update(const Point2i &mousePosition,
+                        const Point2i &lastMousePosition); // Virtual for widget_list: to remove!
 
-  virtual void Draw(const Point2i &mousePosition) const = 0;
-  virtual void NeedRedrawing() { need_redrawing = true; }; // set need_redrawing to true; -- virtual for widget_list
+    virtual void Draw(const Point2i & mousePosition) const = 0;
+    virtual void NeedRedrawing() { need_redrawing = true; }; // set need_redrawing to true; -- virtual for widget_list
 
-  virtual bool SendKey(const SDL_keysym&) { return false; };
-  virtual Widget* Click(const Point2i &mousePosition, uint button);
-  virtual Widget* ClickUp(const Point2i &mousePosition, uint button);
-  const std::string & GetActionName(void) const { return this->actionName; };
-  void SetActionName(const std::string & _actionName) { this->actionName = _actionName; };
+    virtual bool SendKey(const SDL_keysym &) { return false; };
+    virtual Widget * Click(const Point2i & mousePosition, uint button);
+    virtual Widget * ClickUp(const Point2i & mousePosition, uint button);
+    const std::string & GetActionName(void) const { return this->actionName; };
+    void SetActionName(const std::string & _actionName) { this->actionName = _actionName; };
 
-  // widget may be hidden
-  void SetVisible(bool _visible);
-  bool Contains(const Point2i& point) const; // always false if !visible
+    // widget may be hidden
+    void SetVisible(bool _visible);
+    bool Contains(const Point2i & point) const; // always false if !visible
 
-  // manage mouse/keyboard focus
-  bool HasFocus() const { return has_focus; };
-  void SetFocus(bool focus);
+    // manage mouse/keyboard focus
+    bool HasFocus() const { return has_focus; };
+    void SetFocus(bool focus);
 
-  bool IsHighlighted() const;
-  void SetHighlighted(bool focus);
+    bool IsHighlighted() const;
+    void SetHighlighted(bool focus);
 
-  // border, background color
-  void SetBorder(const Color &border_color, uint boder_size);
-  const Color& GetBorderColor() const { return border_color; };
+    // border, background color
+    void SetBorder(const Color & border_color, uint boder_size);
+    const Color & GetBorderColor() const { return border_color; };
 
-  void SetBackgroundColor(const Color &background_color);
-  const Color& GetBackgroundColor() const { return background_color; };
+    void SetBackgroundColor(const Color &background_color);
+    const Color& GetBackgroundColor() const { return background_color; };
 
-  void SetHighlightBgColor(const Color &highlight_bg_color);
-  const Color& GetHighlightBgColor() const { return highlight_bg_color; };
+    void SetHighlightBgColor(const Color &highlight_bg_color);
+    const Color& GetHighlightBgColor() const { return highlight_bg_color; };
 
-  // font color
-  // If (update_now == true), we call OnFontChange()
-  void SetFont(const Color &font_color,
-	       const Font::font_size_t font_size,
-	       const Font::font_style_t font_style,
-	       bool font_shadowed,
-	       bool update_now = true,
-               const Color & shadowColor = black_color);
+    // font color
+    // If (update_now == true), we call OnFontChange()
+    void SetFont(const Color &font_color,
+                 const Font::font_size_t font_size,
+                 const Font::font_style_t font_style,
+                 bool font_shadowed,
+                 bool update_now = true,
+                 const Color & shadowColor = black_color);
 
-  void SetContainer(Container * _ct) { ct = _ct; };
+    void SetContainer(Container * _ct) { ct = _ct; };
 
-  // to manage browsing between the widgets with keyboard
-  virtual Widget* GetFirstWidget() const { return NULL; };
-  virtual Widget* GetLastWidget() const { return NULL; };
-  virtual Widget* GetNextWidget(const Widget */*w*/, bool /*loop*/) const { return NULL; };
-  virtual Widget* GetPreviousWidget(const Widget */*w*/, bool /*loop*/) const { return NULL; };
-  virtual bool IsWidgetBrowser() const { return false; };
+    // to manage browsing between the widgets with keyboard
+    virtual Widget * GetFirstWidget() const { return NULL; };
+    virtual Widget * GetLastWidget() const { return NULL; };
+    virtual Widget * GetNextWidget(const Widget */*w*/, bool /*loop*/) const { return NULL; };
+    virtual Widget * GetPreviousWidget(const Widget */*w*/, bool /*loop*/) const { return NULL; };
+    virtual bool IsWidgetBrowser() const { return false; };
 
-  virtual void Pack() = 0;
+    virtual void Pack() = 0;
 };
 
 #endif
