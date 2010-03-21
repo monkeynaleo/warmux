da212b2a86c6e270bda6bf1c224073f598ac445e
iflo <iflo@30ef787d-52ff-0310-b286-e08351bb7647>
2010-03-21 16:47:15 +0000

    Improve the fixedpoint library.

--- branches/fixedpoint/lib/fixedpoint/fixed_class.h
+++ branches/fixedpoint/lib/fixedpoint/fixed_class.h
@@ -1,4 +1,7 @@
 /*
+Copyright (c) 2010, Wormux Team
+
+Large parts of the code are from the fixed point library of Markus Trenkwalder
 Copyright (c) 2007, Markus Trenkwalder
 
 All rights reserved.
@@ -38,6 +41,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endif
 
 #include "fixed_func.h"
+#include <sstream>
 
 namespace fixedpoint {
 
@@ -47,37 +51,50 @@ namespace fixedpoint {
 template <int p>
 struct fixed_point {
 	int32_t intValue;
-	
+
 	fixed_point() {}
 	/*explicit*/ fixed_point(int32_t i) : intValue(i << p) {}
 	/*explicit*/ fixed_point(float f) : intValue(float2fix<p>(f)) {}
 	/*explicit*/ fixed_point(double f) : intValue(float2fix<p>((float)f)) {}
-	
+	/*explicit*/ fixed_point(long int l) : intValue(l << p) {}
+	/*explicit*/ fixed_point(unsigned int l) : intValue(l << p) {}
+
 	fixed_point& operator += (fixed_point r) { intValue += r.intValue; return *this; }
 	fixed_point& operator -= (fixed_point r) { intValue -= r.intValue; return *this; }
 	fixed_point& operator *= (fixed_point r) { intValue = fixmul<p>(intValue, r.intValue); return *this; }
 	fixed_point& operator /= (fixed_point r) { intValue = fixdiv<p>(intValue, r.intValue); return *this; }
-	
+
 	fixed_point& operator *= (int32_t r) { intValue *= r; return *this; }
 	fixed_point& operator /= (int32_t r) { intValue /= r; return *this; }
-	
+
 	fixed_point operator - () const { fixed_point x; x.intValue = -intValue; return x; }
 	fixed_point operator + (fixed_point r) const { fixed_point x = *this; x += r; return x;}
 	fixed_point operator - (fixed_point r) const { fixed_point x = *this; x -= r; return x;}
 	fixed_point operator * (fixed_point r) const { fixed_point x = *this; x *= r; return x;}
 	fixed_point operator / (fixed_point r) const { fixed_point x = *this; x /= r; return x;}
-	
+
 	bool operator == (fixed_point r) const { return intValue == r.intValue; }
+	bool operator == (int i) const { return intValue == (i << p); }
 	bool operator != (fixed_point r) const { return !(*this == r); }
 	bool operator <  (fixed_point r) const { return intValue < r.intValue; }
+	bool operator < (int i) const { return intValue < (i << p); }
 	bool operator >  (fixed_point r) const { return intValue > r.intValue; }
+	bool operator > (int i) const { return intValue > (i << p); }
 	bool operator <= (fixed_point r) const { return intValue <= r.intValue; }
+	bool operator <= (int i) const { return intValue <= (i << p); }
 	bool operator >= (fixed_point r) const { return intValue >= r.intValue; }
+	bool operator >= (int i) const { return intValue >= (i << p); }
 
 	fixed_point operator + (int32_t r) const { fixed_point x = *this; x += r; return x;}
 	fixed_point operator - (int32_t r) const { fixed_point x = *this; x -= r; return x;}
 	fixed_point operator * (int32_t r) const { fixed_point x = *this; x *= r; return x;}
 	fixed_point operator / (int32_t r) const { fixed_point x = *this; x /= r; return x;}
+	fixed_point operator + (unsigned int r) const { fixed_point x = *this; x += r; return x;}
+	fixed_point operator - (unsigned int r) const { fixed_point x = *this; x -= r; return x;}
+	fixed_point operator * (unsigned int r) const { fixed_point x = *this; x *= r; return x;}
+	fixed_point operator / (unsigned int r) const { fixed_point x = *this; x /= r; return x;}
+
+	operator int() const { return intValue / (1<< 16); }
 };
 
 // Specializations for use with plain integers
@@ -97,6 +114,138 @@ template <int p>
 inline fixed_point<p> operator / (int32_t a, fixed_point<p> b)
 { fixed_point<p> r(a); r /= b; return r; }
 
+
+
+template <int p>
+inline fixed_point<p> operator + (unsigned int a, fixed_point<p> b)
+{ return b + a; }
+
+template <int p>
+inline fixed_point<p> operator - (unsigned int a, fixed_point<p> b)
+{ return -b + a; }
+
+template <int p>
+inline fixed_point<p> operator * (unsigned int a, fixed_point<p> b)
+{ return b * a; }
+
+template <int p>
+inline fixed_point<p> operator / (unsigned int a, fixed_point<p> b)
+{ fixed_point<p> r(a); r /= b; return r; }
+
+template <int p>
+inline fixed_point<p> round(fixed_point<p> r)
+{
+	fixed_point<p> result = r>0? 0.5:-0.5;
+	result += r;
+	result /= (1<< p);
+	return result.intValue;
+}
+
+namespace detail {
+	static bool isDigit(int c) {
+		return c >= '0' &&  c <= '9';
+	}
+}
+
+template <int p>
+std::istream & operator>> (std::istream & is, fixed_point<p> & r)
+{
+	std::stringstream buffer;
+	int next = is.peek();
+	bool minus = (next == '-');
+	if (minus) {
+		buffer << "-";
+		is.get();// skip one character
+	}
+	buffer << '0'; // assuming a leading zero makes it possible to parse numbers starting with a dot and parses "" as 0.
+	next = is.peek();  
+	while (next != is.eof() && detail::isDigit(next)) {
+		char c = is.get();
+		buffer << c;
+		next = is.peek();
+	}
+	int number;
+	buffer >> number;
+	r = number;
+	next = is.peek();
+	if (next == '.') {
+		is.get();// skip point character
+		next = is.peek();
+		fixed_point<p> factor = 1;
+		fixed_point<p> ten = 10;
+		while (next != is.eof() && detail::isDigit(next)) {
+			factor /= ten;     
+			char c;
+			is >> c;
+			int digit = c - '0';
+			if (minus)
+				r -= digit * factor; 
+			else
+				r += digit * factor; 
+			buffer << c;
+			next = is.peek();
+		}
+	}
+
+	return is;
+}
+
+
+template <int p>
+void printTo(std::ostream & os, const fixed_point<p> & r, int digits = -1)
+{
+	if (digits == -1)
+		digits = p / 4;
+	fixed_point<p> rounding_correction = 0.5;
+	fixed_point<p> ten = 10;
+	for (int k = 0; k < digits; k++) {
+		rounding_correction = rounding_correction / ten; 
+	}
+	if (r < 0)
+		rounding_correction = - rounding_correction;
+	fixed_point<p> to_print = r + rounding_correction;
+
+	int left_of_dot = (int) r;
+	os << left_of_dot;
+	if (digits == 0) {
+		return;
+	}
+	os << ".";
+
+	to_print -= left_of_dot;
+	fixed_point<p> zero = 0;
+
+	int zeros = 0;
+	if (to_print < 0) {
+		to_print = - to_print;
+	}
+	for (int i=0; i<digits; i++) {
+		to_print *= 10;
+		int digit = (int)to_print;
+		to_print = to_print - digit;
+		if (digit == 0) {
+			zeros++;
+		} else {
+			for (int j=0; j<zeros; j++) {
+				os << "0";
+			}
+			zeros = 0;
+			os << digit;
+		}
+	}
+	// show at least one digit after the point:
+	if (zeros == digits) {
+		os << "0";
+	}
+}
+
+template <int p>
+std::ostream & operator<< (std::ostream & os, const fixed_point<p> & r)
+{
+	printTo(os, r);
+	return os;
+}
+
 // math functions
 // no default implementation
 
@@ -105,6 +254,15 @@ inline fixed_point<p> sin(fixed_point<p> a);
 
 template <int p>
 inline fixed_point<p> cos(fixed_point<p> a);
+
+template <int p>
+inline fixed_point<p> acos(fixed_point<p> a);
+
+template <int p>
+inline fixed_point<p> asin(fixed_point<p> a);
+
+template <int p>
+inline fixed_point<p> atan(fixed_point<p> a);
 
 template <int p>
 inline fixed_point<p> sqrt(fixed_point<p> a);
@@ -121,6 +279,13 @@ inline fixed_point<p> abs(fixed_point<p> a)
 	fixed_point<p> r; 
 	r.intValue = a.intValue > 0 ? a.intValue : -a.intValue; 
 	return r; 
+}
+
+template <int p>
+inline fixed_point<p> floor(fixed_point<p> a)
+{
+	fixed_point<p> r = a.intValue >> p;
+	return r;
 }
 
 // specializations for 16.16 format
@@ -141,6 +306,29 @@ inline fixed_point<16> cos(fixed_point<16> a)
 	return r;
 }
 
+template <>
+inline fixed_point<16> acos(fixed_point<16> a)
+{
+	fixed_point<16> r;
+	r.intValue = fixacos16(a.intValue);
+	return r;
+}
+
+template <>
+inline fixed_point<16> asin(fixed_point<16> a)
+{
+	fixed_point<16> r;
+	r.intValue = fixasin16(a.intValue);
+	return r;
+}
+
+template <>
+inline fixed_point<16> atan(fixed_point<16> a)
+{
+	fixed_point<16> r;
+	r.intValue = fixatan16(a.intValue);
+	return r;
+}
 
 template <>
 inline fixed_point<16> sqrt(fixed_point<16> a)
