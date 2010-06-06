8b7f6b75df7cdd0577e91ed771449b402aefc8ba
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-06-06 18:39:55 +0000

    [task #7103] Remove MSG_DEBUG using typeid: needless trace (crash reports would have been obvserved) anyway.

--- lib/wormux/include/WORMUX_singleton.h
+++ lib/wormux/include/WORMUX_singleton.h
@@ -51,9 +51,10 @@ protected:
   static T* singleton;
   ~Singleton()
   {
-    MSG_DEBUG("singleton", "Deleting singleton %p of type %s",
-	      singleton, typeid(T).name());
-    if (singleton == this) singleton = NULL;
+    //MSG_DEBUG("singleton", "Deleting singleton %p of type %s",
+    //          singleton, typeid(T).name());
+    if (singleton == this)
+      singleton = NULL;
   }
 
 public:
@@ -62,8 +63,8 @@ public:
     if (!singleton)
     {
       singleton = new T();
-      MSG_DEBUG("singleton", "Created singleton %p of type %s",
-                singleton, typeid(singleton).name());
+      //MSG_DEBUG("singleton", "Created singleton %p of type %s",
+      //          singleton, typeid(singleton).name());
     }
     return singleton;
   }
