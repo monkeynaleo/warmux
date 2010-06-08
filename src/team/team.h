795702a29dfb449f6716ae34931d85a6212b00be
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-06-08 20:13:22 +0000

    Reindent after previous commit.

--- src/team/team.h
+++ src/team/team.h
@@ -42,32 +42,32 @@ class Profile;
 
 class Team
 {
-  public:
-    typedef std::list<Character>::iterator iterator;
-    typedef std::list<Character>::const_iterator const_iterator;
+public:
+  typedef std::list<Character>::iterator iterator;
+  typedef std::list<Character>::const_iterator const_iterator;
 
-  private:
-    Surface mini_flag;
-    Surface flag;
-    Surface death_flag;
-    Surface big_flag;
-    std::string m_teams_dir; // parent directory hosting the data
-    std::string m_id;
-    std::string m_name;
-    std::string m_player_name;
-    std::string m_sound_profile;
-    std::list<Character> characters;
-    iterator active_character;
-    Weapon *active_weapon;
-    uint nb_characters;
-    uint current_turn;
-    AIPlayer * ai;
-    std::string ai_name;
-    bool remote;
-    bool abandoned;
-    CustomTeam *attached_custom_team;
-    bool LoadCharacters();
-    WeaponsList * weapons_list;
+private:
+  Surface mini_flag;
+  Surface flag;
+  Surface death_flag;
+  Surface big_flag;
+  std::string m_teams_dir; // parent directory hosting the data
+  std::string m_id;
+  std::string m_name;
+  std::string m_player_name;
+  std::string m_sound_profile;
+  std::list<Character> characters;
+  iterator active_character;
+  Weapon *active_weapon;
+  uint nb_characters;
+  uint current_turn;
+  AIPlayer * ai;
+  std::string ai_name;
+  bool remote;
+  bool abandoned;
+  CustomTeam *attached_custom_team;
+  bool LoadCharacters();
+  WeaponsList * weapons_list;
 
 protected:
   Team(XmlReader& doc, Profile* res,
@@ -84,97 +84,97 @@ protected:
   CrossHair        crosshair;
   TeamEnergy       energy;
 
-    void LoadGamingData(WeaponsList * weapons);
-    void UnloadGamingData();
+  void LoadGamingData(WeaponsList * weapons);
+  void UnloadGamingData();
 
-    bool IsSameAs(const Team& other) const;
+  bool IsSameAs(const Team& other) const;
 
   // Manage number of characters
-    void SetNbCharacters(uint howmany);
-    uint GetNbCharacters() const { return nb_characters; }
+  void SetNbCharacters(uint howmany);
+  uint GetNbCharacters() const { return nb_characters; }
 
   // Switch to next worm.
-    void NextCharacter(bool new_turn = false);
-    void PreviousCharacter();
-    void SelectCharacter(const Character * c);
+  void NextCharacter(bool new_turn = false);
+  void PreviousCharacter();
+  void SelectCharacter(const Character * c);
 
   // Prepare turn.
-    void PrepareTurn();
+  void PrepareTurn();
 
   // Access to the character.
-    Character& ActiveCharacter() const;
+  Character& ActiveCharacter() const;
 
-    void DrawEnergy(const Point2i& pos);
-    void Refresh();
-    void RefreshAI();
+  void DrawEnergy(const Point2i& pos);
+  void Refresh();
+  void RefreshAI();
 
   // Change the weapon.
-    void SetWeapon (Weapon::Weapon_type nv_arme);
-    int NbAliveCharacter() const;
+  void SetWeapon (Weapon::Weapon_type nv_arme);
+  int NbAliveCharacter() const;
 
   // Access to the active weapon.
-    Weapon& AccessWeapon() const;
-    const Weapon& GetWeapon() const;
-    Weapon::Weapon_type GetWeaponType() const;
+  Weapon& AccessWeapon() const;
+  const Weapon& GetWeapon() const;
+  Weapon::Weapon_type GetWeaponType() const;
 
   // Init the energy bar of the team.
-    void InitEnergy (uint max);
+  void InitEnergy (uint max);
 
   // Update the energy bar values of the team.
-    void UpdateEnergyBar();
-    TeamEnergy & GetEnergyBar() { return energy; };
+  void UpdateEnergyBar();
+  TeamEnergy & GetEnergyBar() { return energy; };
 
   // Read the total energy of the team.
-    uint ReadEnergy() const;
+  uint ReadEnergy() const;
 
   // Access to data.
-    const std::string& GetName() const { return m_name; }
-    const std::string& GetId() const { return m_id; }
-    const std::string& GetPlayerName() const { return m_player_name; }
-    const std::string& GetSoundProfile() const { return m_sound_profile; }
-    const Surface& GetFlag() const { return flag; }
-    const Surface& GetMiniFlag() const { return mini_flag; }
-    const Surface& GetDeathFlag() const { return death_flag; }
-    const Surface& GetBigFlag() const { return big_flag; }
-    iterator begin();
-    iterator end();
-    Character* FindByIndex(uint index);
+  const std::string& GetName() const { return m_name; }
+  const std::string& GetId() const { return m_id; }
+  const std::string& GetPlayerName() const { return m_player_name; }
+  const std::string& GetSoundProfile() const { return m_sound_profile; }
+  const Surface& GetFlag() const { return flag; }
+  const Surface& GetMiniFlag() const { return mini_flag; }
+  const Surface& GetDeathFlag() const { return death_flag; }
+  const Surface& GetBigFlag() const { return big_flag; }
+  iterator begin();
+  iterator end();
+  Character* FindByIndex(uint index);
 
-    void SetPlayerName(const std::string& player_name) { m_player_name = player_name; };
+  void SetPlayerName(const std::string& player_name) { m_player_name = player_name; };
 
   // Number of ammo for the current selected weapon.
   // (return INFINITE_AMMO is ammo are unlimited !)
-    int ReadNbAmmos() const;
-    int ReadNbAmmos(const Weapon::Weapon_type &weapon_type) const;
-    int& AccessNbAmmos();
+  int ReadNbAmmos() const;
+  int ReadNbAmmos(const Weapon::Weapon_type &weapon_type) const;
+  int& AccessNbAmmos();
 
   // Number of current unit per ammo for the selected weapon.
-    int ReadNbUnits() const;
-    int ReadNbUnits(const Weapon::Weapon_type &weapon_type) const;
-    int& AccessNbUnits();
-    void ResetNbUnits();
+  int ReadNbUnits() const;
+  int ReadNbUnits(const Weapon::Weapon_type &weapon_type) const;
+  int& AccessNbUnits();
+  void ResetNbUnits();
 
-    bool IsAI() const { return ai_name != NO_AI_NAME; }
-    bool IsHuman() const { return !IsAI(); }
-    bool IsLocal() const { return !remote; }
-    bool IsRemote() const { return remote; }
-    bool IsLocalAI() const { return IsLocal() && IsAI(); }
-    bool IsLocalHuman() const { return IsLocal() && IsHuman(); }
+  bool IsAI() const { return ai_name != NO_AI_NAME; }
+  bool IsHuman() const { return !IsAI(); }
+  bool IsLocal() const { return !remote; }
+  bool IsRemote() const { return remote; }
+  bool IsLocalAI() const { return IsLocal() && IsAI(); }
+  bool IsLocalHuman() const { return IsLocal() && IsHuman(); }
 
-    bool IsActiveTeam() const;
+  bool IsActiveTeam() const;
 
-    void SetRemote(bool value) { remote = value; }
-    void SetAIName(const std::string value) { ai_name = value; }
-    const std::string GetAIName() { return ai_name; }
-    void LoadAI();
+  void SetRemote(bool value) { remote = value; }
+  void SetAIName(const std::string value) { ai_name = value; }
+  const std::string GetAIName() { return ai_name; }
+  void LoadAI();
 
   // reset characters number, type_of_player and player name
-    void SetDefaultPlayingConfig();
+  void SetDefaultPlayingConfig();
 
   // Custom team
-    void AttachCustomTeam(CustomTeam*);
-    void Abandon() { abandoned = true; }
-    bool IsAbandoned() { return abandoned; }
+  void AttachCustomTeam(CustomTeam*);
+  void Abandon() { abandoned = true; }
+  bool IsAbandoned() { return abandoned; }
 };
 
 #endif /* TEAM_H */
