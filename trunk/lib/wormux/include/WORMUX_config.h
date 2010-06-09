243b3594feb98f279f0303ead5203f342b2a07a8
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-06-09 16:26:12 +0000

    Correct comment for the file.

--- trunk/lib/wormux/include/WORMUX_config.h
+++ trunk/lib/wormux/include/WORMUX_config.h
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  ******************************************************************************
- * Usefull class to manage the configuration of a team
+ * Proxy header including the actual config header for the host system.
  *****************************************************************************/
 
 #ifndef WORMUX_CONFIG_H
