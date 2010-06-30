ec25d6b959c13764a989e9da8a4390fcbd6ab921
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-06-30 21:21:45 +0000

    Allow to specify alpha/colorkey for resource images from code.

--- trunk/src/tool/resource_manager.cpp
+++ trunk/src/tool/resource_manager.cpp
@@ -243,11 +243,11 @@ std::string ResourceManager::LoadImageFilename(const Profile *profile, const std
   return profile->relative_path+filename;
 }
 
-Surface ResourceManager::LoadImage(const Profile *profile, const std::string& resource_name) const
+Surface ResourceManager::LoadImage(const Profile *profile, const std::string& resource_name,
+                                   bool alpha, bool set_colorkey, Uint32 colorkey) const
 {
   std::string    filename = LoadImageFilename(profile, resource_name);
-  bool           alpha    = true;
-  Surface        image    = LoadImage(filename, alpha);
+  Surface        image    = LoadImage(filename, alpha, set_colorkey, colorkey);
   const xmlNode *elem     = GetElement(profile, "surface", resource_name);
   std::string    size;
 
