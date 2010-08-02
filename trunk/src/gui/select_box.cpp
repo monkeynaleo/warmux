9c7ba3d4068826921c27a8c00f54271f511d6564
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-08-02 19:47:00 +0000

    Add a SelectBox which adds selection abilities on top of ScrollBox.

---
+++ trunk/src/gui/select_box.cpp
@@ -0,0 +1,144 @@
+/******************************************************************************
+ *  Wormux is a convivial mass murder game.
+ *  Copyright (C) 2001-2010 Wormux Team.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ ******************************************************************************
+ * Vertical Scroll Box with selection
+ *****************************************************************************/
+
+#include "graphic/video.h"
+#include "gui/select_box.h"
+#include "gui/vertical_box.h"
+#include "include/app.h"
+
+SelectBox::SelectBox(const Point2i& size, bool always)
+  : ScrollBox(size)
+  , selected_item_color(defaultListColor2)
+  , default_item_color(defaultListColor3)
+  , always_one_selected(always)
+  , selected_item(-1)
+{
+  vbox->SetMargin(0);
+}
+
+void SelectBox::Update(const Point2i& mousePosition,
+                       const Point2i& lastMousePosition)
+{
+  Surface& surf = GetMainWindow();
+
+  Rectanglei clip = GetClipRect();
+  SwapWindowClip(clip);
+
+  if (selected_item != -1) {
+    Widget *sel = m_items[selected_item];
+    surf.BoxColor(*sel, selected_item_color);
+  }
+
+  ScrollBox::Update(mousePosition, lastMousePosition);
+
+  int item = MouseIsOnWhichItem(mousePosition);
+  if (item!=-1 && item!=selected_item) {
+    surf.BoxColor(*(m_items[item]), default_item_color);
+  }
+
+  SwapWindowClip(clip);
+}
+
+void SelectBox::Draw(const Point2i &mousePosition) const
+{
+  Surface& surf = GetMainWindow();
+
+  Rectanglei clip = GetClipRect();
+  SwapWindowClip(clip);
+
+  if (selected_item != -1) {
+    Widget *sel = m_items[selected_item];
+    surf.BoxColor(*sel, selected_item_color);
+  }
+
+  ScrollBox::Draw(mousePosition);
+
+  int item = MouseIsOnWhichItem(mousePosition);
+  if (item!=-1 && item!=selected_item) {
+    surf.BoxColor(*(m_items[item]), default_item_color);
+  }
+
+  SwapWindowClip(clip);
+}
+
+Widget * SelectBox::ClickUp(const Point2i & mousePosition, uint button)
+{
+  if (Mouse::IS_CLICK_BUTTON(button)) {
+    int item = MouseIsOnWhichItem(mousePosition);
+
+    if (item!=-1) {
+      if (item==selected_item && !always_one_selected) {
+        Deselect();
+      } else {
+        Select(item);
+      }
+
+      return this;
+    }
+  }
+
+  return ScrollBox::ClickUp(mousePosition, button);
+}
+
+void SelectBox::AddWidgetItem(bool select, Widget* w)
+{
+  AddWidget(w);
+  if (select)
+    Select(m_items.size()-1);
+}
+
+void SelectBox::Select(uint index)
+{
+  ASSERT(index < m_items.size());
+  selected_item = index;
+  NeedRedrawing();
+}
+
+void SelectBox::Deselect()
+{
+  ASSERT(always_one_selected == false);
+  selected_item = -1;
+  NeedRedrawing();
+}
+
+void SelectBox::RemoveSelected()
+{
+  ASSERT (always_one_selected == false);
+
+  if (selected_item != -1) {
+    m_items.erase(m_items.begin() + selected_item);
+    selected_item =- 1;
+  }
+  NeedRedrawing();
+}
+
+int SelectBox::MouseIsOnWhichItem(const Point2i & mousePosition) const
+{
+  if (!Contains(mousePosition)) {
+    return -1;
+  }
+
+  for (uint i=0; i<m_items.size(); i++) {
+    if (m_items[i]->Contains(mousePosition))
+      return i;
+  }
+  return -1;
+}
---
+++ trunk/src/gui/select_box.h
@@ -0,0 +1,91 @@
+/******************************************************************************
+ *  Wormux is a convivial mass murder game.
+ *  Copyright (C) 2001-2010 Wormux Team.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ ******************************************************************************
+ * Vertical Scroll Box with selection
+ *****************************************************************************/
+
+#ifndef SELECT_BOX
+#define SELECT_BOX
+
+#include "gui/scroll_box.h"
+
+class SelectBox : public ScrollBox
+{
+protected:
+  // Colors
+  Color selected_item_color;
+  Color default_item_color;
+
+  // Selection
+  std::vector<Widget*> m_items;
+  bool always_one_selected;
+  int  selected_item;
+
+  int MouseIsOnWhichItem(const Point2i & mousePosition) const;
+
+public:
+  SelectBox(const Point2i& size, bool always_one_selected = true);
+
+  virtual void Draw(const Point2i& mousePosition) const;
+  virtual void Update(const Point2i& mousePosition,
+                      const Point2i& lastMousePosition);
+  virtual Widget* ClickUp(const Point2i & mousePosition, uint button);
+
+  // to add a widget
+  virtual void RemoveSelected();
+  virtual void AddWidget(Widget* w) { m_items.push_back(w); ScrollBox::AddWidget(w); }
+  virtual void AddWidgetItem(bool select, Widget* w);
+  virtual void Empty() { m_items.clear(); ScrollBox::Empty(); }
+
+  // Specific to selection
+  Widget* GetSelectedItem() const { return (selected_item==-1) ? NULL : m_items[selected_item]; };
+  int Size() { return m_items.size(); }
+
+  void SetSelectedItemColor(const Color& color) { selected_item_color = color; };
+  void SetDefaultItemColor(const Color& color) { default_item_color = color; };
+  void Select(uint index);
+  void Deselect();
+  Widget* MouseIsOnWhichWidget(const Point2i & mousePosition) const
+  {
+    int index = MouseIsOnWhichItem(mousePosition);
+    return (index==-1) ? NULL : m_items[selected_item];
+  }
+};
+
+class ItemBox : public SelectBox
+{
+  std::vector<const char*> m_values;
+public:
+  ItemBox(const Point2i& size, bool always = false) : SelectBox(size, always) { };
+  void AddWidget(Widget* w) { SelectBox::AddWidget(w); m_values.push_back(""); }
+  void AddItem(bool select, Widget* w, const char* value)
+  { SelectBox::AddWidgetItem(select, w); m_values.push_back(value); }
+  void RemoveSelected()
+  {
+    if (selected_item != -1) {
+      SelectBox::RemoveSelected();
+      m_values.erase(m_values.begin() + selected_item);
+    }
+  }
+  void Empty() { m_values.clear(); SelectBox::Empty(); }
+
+  const char* GetSelectedValue()
+  { return (selected_item==-1) ? "" : m_values[selected_item]; };
+};
+
+#endif //SELECT_BOX
