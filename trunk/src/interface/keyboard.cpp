de26053b874b844d9fad281a503a59381d6711d8
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-08-13 16:17:46 +0000

    Use the iterator provided by find rather than using the "at" operator: not all STL implementations supports it.

--- trunk/src/interface/keyboard.cpp
+++ trunk/src/interface/keyboard.cpp
@@ -64,12 +64,13 @@ Keyboard::GetRegisteredAction(int raw_key_code, bool ctrl, bool alt, bool shift)
                  alt*ALT_OFFSET + ctrl*CONTROL_OFFSET;
 
   // If the map doesn't have such key, exit immediately
-  if (layout.find(key_code) == layout.end())
+  std::map<int, std::vector<Key_t> >::const_iterator it = layout.find(key_code);
+  if (it == layout.end())
     return KEY_NONE;
 
   // Must use at because [] has side effect of inserting a new key,
   // despite our previous verification
-  return (layout.at(key_code).size()) ? layout.at(key_code)[0] : KEY_NONE;
+  return ((*it).size()) ?(*it)[0] : KEY_NONE;
 }
 
 bool Keyboard::SaveKeyEvent(Key_t at, int raw_key_code,
@@ -77,7 +78,8 @@ bool Keyboard::SaveKeyEvent(Key_t at, int raw_key_code,
 {
   int key_code = raw_key_code + shift*SHIFT_OFFSET +
                  alt*ALT_OFFSET + ctrl*CONTROL_OFFSET;
-  if (layout.find(key_code) == layout.end() || layout.at(key_code).size())
+  std::map<int, std::vector<Key_t> >::const_iterator it = layout.find(key_code);
+  if (it == layout.end() || (*it).size())
     return false;
   SetKeyAction(key_code, at);
   return true;
