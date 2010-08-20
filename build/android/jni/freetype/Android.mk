d3c2c73f0b1845717dcccc3d9de646e7ba397963
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-08-20 21:40:15 +0000

    Add the Android build system, containing stripped down (docs, license, unnecessary code removed) and preconfigured libraries.
    To be update with licenses and most up-to-date code later.

---
+++ build/android/jni/freetype/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := freetype
+
+APP_SUBDIRS := src/autofit src/lzw src/smooth src/cache src/tools \
+               src/base src/gzip src/pcf src/sfnt src/truetype
+#$(patsubst $(LOCAL_PATH)/%, %, $(shell find $(LOCAL_PATH)/src -type d))
+
+# Add more subdirs here, like src/subdir1 src/subdir2
+
+LOCAL_CFLAGS := $(foreach D, $(APP_SUBDIRS), -I$(LOCAL_PATH)/$(D)) \
+                -I$(LOCAL_PATH)/include -DFT2_BUILD_LIBRARY
+
+#Change C++ file extension as appropriate
+LOCAL_CPP_EXTENSION := .cpp
+
+LOCAL_SRC_FILES := $(foreach F, $(APP_SUBDIRS), $(addprefix $(F)/,$(notdir $(wildcard $(LOCAL_PATH)/$(F)/*.c))))
+LOCAL_LDLIBS := -lz
+
+include $(BUILD_STATIC_LIBRARY)
