1de07f27f3406121cec76ef3bd05b834e7dd413d
mikkov <mikkov@30ef787d-52ff-0310-b286-e08351bb7647>
2010-10-04 15:43:09 +0000

    disable again fullscreen option for HAVE_HANDHELD

--- trunk/src/menu/options_menu.cpp
+++ trunk/src/menu/options_menu.cpp
@@ -107,11 +107,11 @@ OptionMenu::OptionMenu() :
     new PictureTextCBox(_("Player's name?"), "menu/display_name", option_size);
   graphic_options->AddWidget(opt_display_name);
 
-
+#ifndef HAVE_HANDHELD
   full_screen =
     new PictureTextCBox(_("Fullscreen?"), "menu/fullscreen", option_size);
   graphic_options->AddWidget(full_screen);
-
+#endif
 
   opt_max_fps =
     new SpinButtonWithPicture(_("Maximum FPS"), "menu/fps",
@@ -315,7 +315,9 @@ OptionMenu::OptionMenu() :
 #endif
   opt_display_energy->SetValue(config->GetDisplayEnergyCharacter());
   opt_display_name->SetValue(config->GetDisplayNameCharacter());
+#ifndef HAVE_HANDHELD
   full_screen->SetValue(app->video->IsFullScreen());
+#endif
   music_cbox->SetValue(config->GetSoundMusic());
   effects_cbox->SetValue(config->GetSoundEffects());
   warn_cbox->SetValue(config->GetWarnOnNewPlayer());
