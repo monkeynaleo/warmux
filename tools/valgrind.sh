23cf781782523a27e47ebbeca8ae07c485d8e47f
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-10-06 20:32:04 +0000

    Try to force STL containers to release the memory from pools. Doesn't seem to help reducing strange errors.

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -4,7 +4,7 @@ if [ "$1" = "" ]; then
     exit
 fi
 
-LOG=valgrind.log
-valgrind --tool=memcheck --show-reachable=yes --log-file=$LOG \
-    --leak-check=yes --leak-resolution=high --num-callers=12 \
-    --suppressions=$(dirname $0)/wormux.supp $* -f
+GLIBCXX_FORCE_NEW=1 valgrind \
+  --tool=memcheck --show-reachable=yes --log-file=valgrind.log \
+  --leak-check=full --leak-resolution=high --num-callers=12 \
+  --suppressions=$(dirname $0)/wormux.supp $* -f
