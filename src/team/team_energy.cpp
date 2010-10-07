0182edaf37c171e553612c0b8a7f1cc5db80dc2c
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-10-07 18:02:17 +0000

    Fix a Sprite memory leak in TeamEnergy

--- src/team/team_energy.cpp
+++ src/team/team_energy.cpp
@@ -108,7 +108,6 @@ void TeamEnergy::Config(uint _current_energy,
   new_value = _current_energy;
   ASSERT(max_value != 0)
   energy_bar->InitVal(value, 0, max_value, ProgressBar::PROG_BAR_VERTICAL);
-  icon = NULL;
   SetIcon(team->GetFlag());
   energy_list.Reset();
 }
