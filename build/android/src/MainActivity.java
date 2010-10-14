fab3cbd239a510b8a21c838bffbaf1dfd4584655
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-10-14 20:09:30 +0000

    Force screen to be kept on. This is not cool battery wise, but I can't get the application to properly go to background.

--- build/android/src/MainActivity.java
+++ build/android/src/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends Activity {
 
     // fullscreen mode
     requestWindowFeature(Window.FEATURE_NO_TITLE);
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-           WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN|WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                         WindowManager.LayoutParams.FLAG_FULLSCREEN|WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     _tv = new TextView(this);
     _tv.setText(R.string.init);
