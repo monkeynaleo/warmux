5d1913349ebcdbfb74e5488a2cfa73c712df0d71
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-10-23 18:38:28 +0000

    Rework the way one sets the captions. The old way saved nothing.

--- trunk/src/menu/help_menu.cpp
+++ trunk/src/menu/help_menu.cpp
@@ -47,79 +47,65 @@ HelpMenu::HelpMenu()
   MultiTabs * tabs = new MultiTabs(Point2i(max_w, max_h));
   tabs->SetPosition(border, border);
 
-#define DEF_KEY_CAPTIONS_PARAMS 132, (Font::font_size_t)14, DEF_CAPTIONS_PARAMS
-  static const FigureWidget::Caption controls_captions[] = {
-    { _("Quit game"), 81, 13, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("High jump"), 439, 326, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Jump backwards"), 439, 297, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Jump backwards"), 439, 356, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Drag&drop: Move camera"), 520, 395, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Center camera on character"), 520, 422, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Show/hide the interface"), 271, 43, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Toggle fullscreen"), 491, 42, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Configuration menu"), 491, 72, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Talk in network battles"), 92, 296, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Change weapon category"), 81, 72, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Change weapon countdown"), 618, 164, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Change aim angle"), 618, 194, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Move character"), 618, 286, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("On map: select a target"), 618, 225, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("On a character: select it"), 618, 256, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Show weapons menu"), 618, 134, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Smaller aim angle and walk step"), 92, 326, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Move camera with mouse or arrows"), 386, 394, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Weapon: Fire / Bonus box: drop"), 260, 326, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Toggle minimap"), 271, 72, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Change active character"), 92, 356, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Center camera on character"), 386, 422, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Quickly quit game with Ctrl"), 81, 43, DEF_KEY_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-  };
-  Widget *w = new FigureWidget(Point2i(max_w,
-                                       tabs->GetSizeY() - tabs->GetHeaderHeight()),
-                               "help/shortkeys",
-                               controls_captions, ARRAY_SIZE(controls_captions),
-                               PictureWidget::FIT_SCALING);
+  FigureWidget *w = new FigureWidget(Point2i(max_w,
+                                             tabs->GetSizeY() - tabs->GetHeaderHeight()),
+                                     "help/shortkeys", (Font::font_size_t)14);
+  w->AddCaption(_("Quit game"), 81, 13, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("High jump"), 439, 326, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Jump backwards"), 439, 297, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Jump backwards"), 439, 356, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Drag&drop: Move camera"), 520, 395, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Center camera on character"), 520, 422, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Show/hide the interface"), 271, 43, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Toggle fullscreen"), 491, 42, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Configuration menu"), 491, 72, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Talk in network battles"), 92, 296, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Change weapon category"), 81, 72, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Change weapon countdown"), 618, 164, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Change aim angle"), 618, 194, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Move character"), 618, 286, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("On map: select a target"), 618, 225, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("On a character: select it"), 618, 256, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Show weapons menu"), 618, 134, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Smaller aim angle and walk step"), 92, 326, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Move camera with mouse or arrows"), 386, 394, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Weapon: Fire / Bonus box: drop"), 260, 326, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Toggle minimap"), 271, 72, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Change active character"), 92, 356, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Center camera on character"), 386, 422, 132); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Quickly quit game with Ctrl"), 81, 43, 132); // TRANSLATORS: please be imaginative and keep it short!
   tabs->AddNewTab("unused", _("Keyboard"), w);
   widgets.AddWidget(tabs);
 
-# define DEF_STARTGAME_CAPTIONS_PARAMS 238, (Font::font_size_t)22, DEF_CAPTIONS_PARAMS
-  static const FigureWidget::Caption startgame_captions[] = {
-    { _("Click to change team selected"), 293, 141, DEF_STARTGAME_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Edit player name"), 541, 141, DEF_STARTGAME_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Go to game rules editing submenu"), 788, 141, DEF_STARTGAME_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Click to select AI/human team"), 325, 275, DEF_STARTGAME_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Change per team number of players"), 605, 275, DEF_STARTGAME_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Change number of playing teams"), 146, 334, DEF_STARTGAME_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Click/wheelmouse to change map"), 466, 334, DEF_STARTGAME_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-  };
   w = new FigureWidget(Point2i(max_w,
                                tabs->GetSizeY() - tabs->GetHeaderHeight()),
-                       "help/startgame_menu",
-                       startgame_captions, ARRAY_SIZE(startgame_captions),
-                       PictureWidget::FIT_SCALING);
+                       "help/startgame_menu", (Font::font_size_t)22);
+  w->AddCaption(_("Click to change team selected"), 293, 141, 238); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Edit player name"), 541, 141, 238); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Go to game rules editing submenu"), 788, 141, 238); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Click to select AI/human team"), 325, 275, 238); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Change per team number of players"), 605, 275, 238); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Change number of playing teams"), 146, 334, 238); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Click/wheelmouse to change map"), 466, 334, 238); // TRANSLATORS: please be imaginative and keep it short!
   tabs->AddNewTab("unused", _("Game menu"), w);
 
-# define DEF_GAMEMODE_CAPTIONS_PARAMS 252, Font::FONT_LARGE, DEF_CAPTIONS_PARAMS
-  static const FigureWidget::Caption gamemode_captions[] = {
-    { _("Experiment new game rules"), 206, 45, DEF_GAMEMODE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Increase this if you are a slow player"), 483, 45, DEF_GAMEMODE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("When a character can be switched"), 761, 45, DEF_GAMEMODE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("More energy, more shots needed"), 1038, 45, DEF_GAMEMODE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("If you like medkits, increase this"), 206, 668, DEF_GAMEMODE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Increase this if you like long games"), 482, 668, DEF_GAMEMODE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Increase this for faster death mode end"), 761, 668, DEF_GAMEMODE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("The bigger the faster you'll fall!"), 1042, 668, DEF_GAMEMODE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-  };
   w = new FigureWidget(Point2i(max_w,
                                tabs->GetSizeY() - tabs->GetHeaderHeight()),
-                       "help/game_mode",
-                       gamemode_captions, ARRAY_SIZE(gamemode_captions),
-                       PictureWidget::FIT_SCALING);
+                       "help/game_mode", Font::FONT_LARGE);
+  w->AddCaption(_("Experiment new game rules"), 206, 45, 252);// TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Increase this if you are a slow player"), 483, 45, 252);// TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("When a character can be switched"), 761, 45, 252);// TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("More energy, more shots needed"), 1038, 45, 252);// TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("If you like medkits, increase this"), 206, 668, 252);// TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Increase this if you like long games"), 482, 668, 252);// TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Increase this for faster death mode end"), 761, 668, 252);// TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("The bigger the faster you'll fall!"), 1042, 668, 252);// TRANSLATORS: please be imaginative and keep it short!
   tabs->AddNewTab("unused", _("Game mode"), w);
 
-#define INGAME_CAPTIONS_PARAMS Font::FONT_LARGE, DEF_CAPTIONS_PARAMS
-  static const FigureWidget::Caption ingame_captions[] = {
-    {
+  w = new FigureWidget(Point2i(max_w,
+                               tabs->GetSizeY() - tabs->GetHeaderHeight()),
+                       "help/ingame", Font::FONT_LARGE);
+  w->AddCaption(
 #ifdef ANDROID
       _("To jump, press space or trackball or dpad center.\n"
         "To aim or move more slowly, use shift/vol+.\n"
@@ -133,38 +119,25 @@ HelpMenu::HelpMenu()
         "Click a character from your team to select it.\n"),
       // TRANSLATORS: please be imaginative and keep it short!
 #endif
-      197, 337, 289, INGAME_CAPTIONS_PARAMS },
-    { _("Minimap, more details in another tab"), 543, 132, 330, INGAME_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Gauge to set speed of some projectiles"), 619, 494, 411, INGAME_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Interface, more details in another tab"), 1003, 437, 209, INGAME_CAPTIONS_PARAMS } // TRANSLATORS: please be imaginative and keep it short!
-  };
-  w = new FigureWidget(Point2i(max_w,
-                               tabs->GetSizeY() - tabs->GetHeaderHeight()),
-                       "help/ingame",
-                       ingame_captions, ARRAY_SIZE(ingame_captions),
-                       PictureWidget::FIT_SCALING);
+      197, 337, 289);
+  w->AddCaption(_("Minimap, more details in another tab"), 543, 132, 330); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Gauge to set speed of some projectiles"), 619, 494, 411); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Interface, more details in another tab"), 1003, 437, 209); // TRANSLATORS: please be imaginative and keep it short!
   tabs->AddNewTab("unused", _("Ingame display"), w);
 
-#define INTERFACE_CAPTIONS_PARAMS Font::FONT_LARGE, DEF_CAPTIONS_PARAMS
-  static const FigureWidget::Caption interface_captions[] = {
-    { _("Character name.\nCharacter energy.\nTeam name.\nPlayer name.\n"),
-      163, 101, 302, INTERFACE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Time left in this turn.\nTime elapsed since game start.\n"),
-      473, 101, 262, INTERFACE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Team in ordre of decreasing energy"), 763, 103, 200, INTERFACE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Selected weapon and ammo left"), 352, 635, 255, INTERFACE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Wind strength and direction"), 616, 635, 245, INTERFACE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-    { _("Fire your weapon!"), 866, 635, 221, INTERFACE_CAPTIONS_PARAMS }, // TRANSLATORS: please be imaginative and keep it short!
-  };
   w = new FigureWidget(Point2i(max_w,
                                tabs->GetSizeY() - tabs->GetHeaderHeight()),
-                       "help/interface",
-                       interface_captions, ARRAY_SIZE(interface_captions),
-                       PictureWidget::FIT_SCALING);
+                       "help/interface", Font::FONT_LARGE);
+  w->AddCaption(_("Character name.\nCharacter energy.\nTeam name.\nPlayer name.\n"), 163, 101, 302); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Time left in this turn.\nTime elapsed since game start.\n"), 473, 101, 262); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Team in ordre of decreasing energy"), 763, 103, 200); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Selected weapon and ammo left"), 352, 635, 255); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Wind strength and direction"), 616, 635, 245); // TRANSLATORS: please be imaginative and keep it short!
+  w->AddCaption(_("Fire your weapon!"), 866, 635, 221); // TRANSLATORS: please be imaginative and keep it short!
   tabs->AddNewTab("unused", _("Interface"), w);
 
-  w = new ControlConfig(tabs->GetSize(), true);
-  tabs->AddNewTab("unused", _("Current controls"), w);
+  tabs->AddNewTab("unused", _("Current controls"),
+                  new ControlConfig(tabs->GetSize(), true));
 
   GetResourceManager().UnLoadXMLProfile(res);
   widgets.Pack();
