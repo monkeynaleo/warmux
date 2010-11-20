b6c5f7abae1c1ebac4606153d8c1ce64cb76aea6
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-11-20 20:44:32 +0000

    Don't use memory for caching something as rarely happening as a body particle.

--- branches/sprite-caching/particles/body_member.cpp
+++ branches/sprite-caching/particles/body_member.cpp
@@ -31,7 +31,7 @@ BodyMemberParticle::BodyMemberParticle(Sprite& spr, const Point2i& position) :
   SetCollisionModel(true, false, false);
   m_left_time_to_live = 100;
   image = new Sprite(spr.GetSurface());
-  image->EnableCaches(true, 32, 0, PI);
+  image->EnableCaches(false, 0);
   ASSERT(image->GetWidth() != 0 && image->GetHeight()!=0);
   SetXY(position);
 
