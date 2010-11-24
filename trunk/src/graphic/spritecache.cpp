4574aa52db4d652106f6a2d0722b8af94da9a5bb
gentildemon <gentildemon@30ef787d-52ff-0310-b286-e08351bb7647>
2010-11-24 19:30:17 +0000

    The [] operator used is obvious.
    Imported from: svn+ssh://svn.gna.org/svn/warmux/branches/sprite-caching@8973
    Original author: kurosu
    Original date: Sat Nov 20 15:46:13 2010 +0000

--- trunk/src/graphic/spritecache.cpp
+++ trunk/src/graphic/spritecache.cpp
@@ -80,14 +80,14 @@ void SpriteCache::EnableCaches(bool flipped, uint rotation_num, const Double& mi
   assert(!empty());
 
   for (uint f=0; f<size(); f++) {
-    std::vector<SpriteFrameCache>::operator[](f).SetCaches(flipped, rotation_num, min, max);
+    operator[](f).SetCaches(flipped, rotation_num, min, max);
   }
 }
 
 void SpriteCache::FixParameters(const Double& rotation_rad, const Double& scale_x, const Double& scale_y)
 {
   for (uint i=0; i<size(); i++) {
-    SpriteFrameCache& frame = std::vector<SpriteFrameCache>::operator[](i);
+    SpriteFrameCache& frame = operator[](i);
 #ifdef HAVE_HANDHELD
     frame.normal_surface = frame.normal_surface.RotoZoom(rotation_rad, scale_x, scale_y, true).DisplayFormatColorKey(128);
 #else
