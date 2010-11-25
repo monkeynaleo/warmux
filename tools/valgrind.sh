61c446a9e48e4fd0585003a9b2a63458fef3c957
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-11-25 20:30:27 +0000

    Tweak a bit valgrind script and suppression file (to remove leaks not due to warmux).

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 if [ "$1" = "" ]; then
-    echo "Usage: $0 /path/to/warmux <warmux options>"
+    echo "Usage: $0 <valgrind options> /path/to/warmux <warmux options>"
     exit
 fi
 
+OPTIONS="--show-reachable=yes --leak-check=full --leak-resolution=high"
+if [ "$1" != "src/warmux" ]; then OPTIONS=""; fi
+
 GLIBCXX_FORCE_NEW=1 valgrind \
-  --tool=memcheck --show-reachable=yes --log-file=valgrind.log \
-  --leak-check=full --leak-resolution=high --num-callers=12 \
-  --suppressions=$(dirname $0)/warmux.supp $* -f
+  --tool=memcheck --log-file=valgrind.log --num-callers=12 \
+  --suppressions=$(dirname $0)/warmux.supp $OPTIONS $* -f
