488deb00a867d20cbba7d488ab48f35c09077f68
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-11-27 11:43:36 +0000

    Fix wind particules with incorrect background.

--- src/map/wind.cpp
+++ src/map/wind.cpp
@@ -78,9 +78,9 @@ WindParticle::WindParticle(const std::string &xml_file, Double scale)
   sprite->SetAlpha(scale);
   sprite->SetCurrentFrame(RandomLocal().GetInt(0, sprite->GetFrameCount() - 1));
   SetSize(sprite->GetSize());
-  sprite->FixParameters(true);
 
   bool not_fixed = GetAlignParticleState()|| ActiveMap()->GetWind().rotation_speed.IsNotZero();
+  sprite->FixParameters(!not_fixed);
   sprite->EnableCaches(ActiveMap()->GetWind().need_flip, not_fixed ? 64 : 0);
   if (not_fixed) {
     sprite->SetRotation_rad(RandomLocal().GetInt(0,628)/100.0); // 0 < angle < 2PI
