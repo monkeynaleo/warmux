d7fda81ca5163078ba2862673f94991fa88f20fb
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2010-12-08 13:35:26 +0000

    [Android] Update way of declaring includes, which is not CFLAGS.

--- trunk/build/android/jni/sdl_ttf/Android.mk
+++ trunk/build/android/jni/sdl_ttf/Android.mk
@@ -4,14 +4,14 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := sdl_ttf
 
-LOCAL_CFLAGS := -I$(LOCAL_PATH) -I$(LOCAL_PATH)/../sdl/include \
-                -I$(LOCAL_PATH)/../freetype/include -Os
+LOCAL_CFLAGS := -Os
 
 LOCAL_CPP_EXTENSION := .cpp
 
 # Note this simple makefile var substitution, you can find even simpler examples in different Android projects
 LOCAL_SRC_FILES := SDL_ttf.c
-
+LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/../freetype/include \
+                    $(LOCAL_PATH)/../sdl/include
 LOCAL_STATIC_LIBRARIES := freetype
 LOCAL_SHARED_LIBRARIES := sdl
 LOCAL_LDLIBS := -lz
