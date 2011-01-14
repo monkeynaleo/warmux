9127bc351055f989556ba7a2e208d3f05746ca8d
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2011-01-14 07:20:41 +0000

    Update copyright mention.

--- src/tool/affine_transform.cpp
+++ src/tool/affine_transform.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
 *  Warmux is a convivial mass murder game.
-*  Copyright (C) 2001-2010 Warmux Team.
+*  Copyright (C) 2001-2011 Warmux Team.
 *
 *  This program is free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
