a1cd63259c084caa92845fa5192c5d891fd02e19
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2011-02-04 16:49:09 +0000

    Fix iterating for folders only.

--- lib/warmux/tools/file_tools.cpp
+++ lib/warmux/tools/file_tools.cpp
@@ -191,7 +191,7 @@ const char* FolderSearchNext(FolderSearch *f, bool& file)
 
     // We're not searching for folder, so exit with failure
     if (!file)
-      return NULL;
+      continue;
 
     // This is a file and we do search for files
     file = true;
