10084ac9e6a17d3b1ee57b4224e62a8f3dad2254
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2011-02-22 22:45:09 +0000

    Fix compilation.

--- src/gui/file_list_box.cpp
+++ src/gui/file_list_box.cpp
@@ -157,7 +157,7 @@ void FileListBox::PopulateFileList(const std::string& path)
           filename = new std::string(new_path);
         *filename += name;
         MSG_DEBUG("file", "Adding directory %s\n", name);
-        AddLabelItem(false, "[" + ANSIToUTF8(new_path, name) + "]", filename,
+        AddLabelItem(false, std::string("[") + ANSIToUTF8(new_path, name) + "]", filename,
                      Font::FONT_MEDIUM, Font::FONT_NORMAL, c_yellow);
       } else
         MSG_DEBUG("file", "Rejecting %s\n", name);
