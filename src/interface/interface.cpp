75e91793ec076bbe720ed5ba123d29fa12624f6e
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2011-03-08 18:29:40 +0000

    Allow any click (even for a weapon with selectable target) on the minimap to move the camera. (this is a revert of a previous change)

--- src/interface/interface.cpp
+++ src/interface/interface.cpp
@@ -1134,8 +1134,7 @@ bool Interface::ActionClickUp(const Point2i &mouse_pos, const Point2i &old_click
     }
   }
 
-  if (display_minimap && // We are not targetting
-      Mouse::GetInstance()->GetPointer() == Mouse::POINTER_SELECT) {
+  if (display_minimap) {
     Point2i    offset(window.GetWidth() - GetWorld().ground.GetPreviewSize().x - 2*MARGIN, 2*MARGIN);
     Rectanglei rect_preview(offset, GetWorld().ground.GetPreviewSize());
     if (rect_preview.Contains(mouse_pos)) {
