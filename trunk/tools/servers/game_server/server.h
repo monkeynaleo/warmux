187d082933de150cdc68a7f033d5dba4762aebce
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2011-03-09 19:43:27 +0000

    Add an additional accessor as gcc seems a bit too dumb here to cast (stuff) to (const stuff).

--- trunk/tools/servers/game_server/server.h
+++ trunk/tools/servers/game_server/server.h
@@ -77,6 +77,7 @@ public:
   void CheckWaited();
   void ResetWaiting() { warned = false; waited = NULL; start_waiting = 0; }
   bool IsGameMaster(std::list<DistantComputer*>::const_iterator& cpu) const { return cpu == cpulist.begin(); }
+  bool IsGameMaster(std::list<DistantComputer*>::iterator& cpu) { return cpu == cpulist.begin(); }
 };
 
 class GameServer : public Singleton<GameServer>
