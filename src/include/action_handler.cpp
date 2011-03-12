fdec71e18a891731c557a34aa7ad9f6876c038d4
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2011-03-12 16:08:17 +0000

    [bug #13334] Reject actions strangely received by a client, while the action is only meant for the host.

--- src/include/action_handler.cpp
+++ src/include/action_handler.cpp
@@ -511,9 +511,13 @@ static void _Action_Game_MapList(Action *a)
 
 static void Action_Game_SetMapList(Action *a)
 {
+  Network  *net      = Network::GetInstance();
+  if (!net->IsGameMaster()) {
+    MSG_DEBUG("action_handler.map", "Dropping message from %p that I shouldn't have received...\n", a->GetCreator());
+    return;
+  }
   if (a)
     _Action_Game_MapList(a);
-  Network  *net      = Network::GetInstance();
   MapsList *map_list = MapsList::GetInstance();
 
   // We are the game master: the received list must be used to determine
