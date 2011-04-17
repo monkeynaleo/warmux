3e9de3c8a7b58a4bf119e04e078575bd5e74d618
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2011-04-17 09:17:05 +0000

    Remove the current 11.04 tag as there are further commits in the corresponding branch.

--- tags/warmux-11.04/build/android/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-#APP_PROJECT_PATH := $(call my-dir)/project
-APP_OPTIM := release
-APP_ABI   := armeabi
-LOCAL_ARM_MODE := arm
-APP_STL := stlport_static
-
--- tags/warmux-11.04/data/interface/not_skin/readme.txt
+++
@@ -1,1 +0,0 @@
-These 3 files could be deleted if task 6160 was done. It's a difficult task but important one.
