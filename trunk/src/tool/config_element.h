3d30b82611e02058d984fbaa5588af812abf073d
kurosu <kurosu@30ef787d-52ff-0310-b286-e08351bb7647>
2011-06-06 17:14:32 +0000

    Did not add the files.

---
+++ trunk/src/tool/config_element.h
@@ -0,0 +1,104 @@
+#ifndef CONFIG_ELEMENT_H
+#define CONFIG_ELEMENT_H
+
+#include <list>
+
+#include <WARMUX_base.h>
+#include <WARMUX_error.h>
+
+class XmlWriter;
+typedef struct _xmlNode xmlNode;
+
+class ConfigElement
+{
+public:
+  typedef enum
+  {
+    TYPE_INT,
+    TYPE_UINT,
+    TYPE_BOOL,
+    TYPE_DOUBLE
+  } Type;
+
+  Type        m_type;
+  const char *m_name;
+  bool        m_important;
+
+  virtual bool Read(const xmlNode* father) = 0;
+  virtual void Write(XmlWriter& writer, xmlNode* elem) const = 0;
+
+protected:
+  ConfigElement(Type t, const char *n, bool imp = false)
+  {
+    m_type = t; m_name = n; m_important = imp;
+  }
+};
+
+class IntConfigElement : public ConfigElement
+{
+public:
+  int       *m_val, m_def, m_min, m_max;
+  IntConfigElement(const char *n, int *v, int d)
+    : ConfigElement(TYPE_INT, n, false) {  m_val = v; *v = m_def = d; }
+  IntConfigElement(const char *n, int *v, int d, int mi, int ma)
+    : ConfigElement(TYPE_INT, n, true) { m_val = v; *v = m_def = d; m_min = mi, m_max = ma; ASSERT(d >= mi && d <= ma); }
+  bool Read(const xmlNode* father);
+  void Write(XmlWriter& writer, xmlNode* father) const;
+};
+
+class BoolConfigElement : public ConfigElement
+{
+public:
+  bool       *m_val, m_def;
+  BoolConfigElement(const char *n, bool *v, bool d, bool imp = false)
+    : ConfigElement(TYPE_BOOL, n, imp) { m_val = v; *v = m_def = d; }
+  bool Read(const xmlNode* father);
+  void Write(XmlWriter& writer, xmlNode* father) const;
+};
+
+class UintConfigElement : public ConfigElement
+{
+public:
+  uint       *m_val, m_def, m_min, m_max;
+  UintConfigElement(const char *n, uint *v, uint d)
+    : ConfigElement(TYPE_UINT, n, false) { m_val = v; *v = m_def = d; }
+  UintConfigElement(const char *n, uint *v, uint d, uint mi, uint ma)
+    : ConfigElement(TYPE_UINT, n, true) { m_val = v; *v = m_def = d; m_min = mi, m_max = ma; ASSERT(d >= mi && d <= ma); }
+  bool Read(const xmlNode* father);
+  void Write(XmlWriter& writer, xmlNode* father) const;
+};
+
+class DoubleConfigElement : public ConfigElement
+{
+public:
+  Double       *m_val, m_def, m_min, m_max;
+  DoubleConfigElement(const char *n, Double* v, Double d)
+    : ConfigElement(TYPE_DOUBLE, n, false) { m_val = v; *v = m_def = d; }
+  DoubleConfigElement(const char *n, Double* v, Double d, Double mi, Double ma)
+    : ConfigElement(TYPE_DOUBLE, n, true) { m_val = v; *v = m_def = d; m_min = mi, m_max = ma; ASSERT(d >= mi && d <= ma); }
+  bool Read(const xmlNode* father);
+  void Write(XmlWriter& writer, xmlNode* father) const;
+};
+
+class StringConfigElement : public ConfigElement
+{
+public:
+  std::string       *m_val, m_def;
+  StringConfigElement(const char *n, std::string *v, const std::string& d, bool imp = false)
+    : ConfigElement(TYPE_BOOL, n, imp) { m_val = v; *v = m_def = d; }
+  bool Read(const xmlNode* father);
+  void Write(XmlWriter& writer, xmlNode* father) const;
+};
+
+class ConfigElementList : public std::list<ConfigElement*>
+{
+public:
+  typedef std::list<ConfigElement*>::iterator iterator;
+  typedef std::list<ConfigElement*>::const_iterator const_iterator;
+
+  virtual ~ConfigElementList();
+  void LoadXml(const xmlNode* elem, const char* node = NULL);
+  xmlNode *SaveXml(XmlWriter& writer, xmlNode* elem, const char* node = NULL);
+};
+
+#endif // CONFIG_ELEMENT_H
