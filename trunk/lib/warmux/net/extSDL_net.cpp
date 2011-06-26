c7eaca5919bd03a812e09718b541a7c62751128c
mikkov <mikkov@30ef787d-52ff-0310-b286-e08351bb7647>
2011-06-26 15:57:51 +0000

    include string.h explicitly for strerror

--- trunk/lib/warmux/net/extSDL_net.cpp
+++ trunk/lib/warmux/net/extSDL_net.cpp
@@ -19,6 +19,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 =============================================================================*/
 
 #include <errno.h>
+#ifndef WIN32
+#include <string.h>
+#endif
 
 /******************************************************************************
  * From SDLNet 1.2.7 - SDLnetsys.h
